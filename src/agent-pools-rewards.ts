bd65da475261fb6b18bd2e0fc48d192c4864bbeb
Artyom Veremeenko <arwer13@gmail.com>
2021-12-08 16:50:49 +0300

    fixes and refactoring of pools rewards agent

--- src/agent-pools-rewards.ts
+++ src/agent-pools-rewards.ts
@@ -1,10 +1,8 @@
-import {readFileSync} from 'fs'
-import BigNumber from 'bignumber.js'
+import { strict as assert } from 'assert'
 
 import {
   ethers,
   BlockEvent,
-  TransactionEvent,
   Finding,
   FindingType,
   FindingSeverity,
@@ -15,8 +13,9 @@ import {ethersProvider} from './ethers'
 
 import {
   LDO_TOKEN_ADDRESS,
-  POOL_REWARDS_STILL_NOT_PROLONGED_ALERT_PERIOD,
-  REWARDS_ALERT_PARAMETERS,
+  MAX_DELAY_OF_POOL_REWARDS_PERIOD_PROLONGATION,
+  POOL_REWARDS_ALERTS_PERIODS_PARAMS,
+  POOLS_PARAMS,
 } from './constants'
 
 
@@ -27,7 +26,6 @@ import CURVE_MANAGER_ABI from './abi/CurveManager.json'
 import CURVE_REWARDS_ABI from './abi/CurveRewards.json'
 import BALANCER_MANAGER_ABI from './abi/BalancerManager.json'
 import ONE_INCH_MANAGER_ABI from './abi/OneInchManager.json'
-
 const rewardContractAbis = {
   Sushi: {
     manager: SUSHI_MANAGER_ABI,
@@ -60,6 +58,10 @@ const ldoToken = new ethers.Contract(LDO_TOKEN_ADDRESS, LDO_TOKEN_ABI, ethersPro
 let g_pools = {} as any
 
 
+function arePeriodsSorted(periods: any) {
+  return periods.every((v: any, i: number, arr: any) => !i || arr[i-1].period <= v.period)
+}
+
 async function readPeriodFinish(poolName: string) {
   try {
     return +(await g_pools[poolName].manager.period_finish()).toString()
@@ -70,49 +72,49 @@ async function readPeriodFinish(poolName: string) {
 
 export async function initialize(currentBlock: number): Promise<{[key: string]: string}> {
   console.log(`[${name}]`)
+  assert(arePeriodsSorted(POOL_REWARDS_ALERTS_PERIODS_PARAMS), 'Alert periods parameters must be sorted by period ascending')
 
   let metadata: {[key: string]: string} = {}
 
-  for (const [name, config] of Object.entries(REWARDS_ALERT_PARAMETERS)) {
-    g_pools[name] = {
-      managerAddress: config.managerAddress,
-      rewardsAddress: config.rewardsAddress,
-      alerts: config.alerts,
-      manager: new ethers.Contract(config.managerAddress, rewardContractAbis[name].manager, ethersProvider),
+  for (const [poolName, poolParams] of Object.entries(POOLS_PARAMS)) {
+    g_pools[poolName] = {
+      ...poolParams,
+      manager: new ethers.Contract(poolParams.managerAddress, rewardContractAbis[poolName].manager, ethersProvider),
       rewards: undefined,
       periodFinish: 0,
       periodExpired: false,
       lastNotification: 0,
     }
-    if (config.rewardsAddress) {
-      g_pools[name]['rewards'] = new ethers.Contract(config.rewardsAddress, rewardContractAbis[name].rewards, ethersProvider)
+
+    if (poolParams.rewardsAddress) {
+      g_pools[poolName]['rewards'] = new ethers.Contract(poolParams.rewardsAddress, rewardContractAbis[poolName].rewards, ethersProvider)
     }
 
-    const periodFinish = await readPeriodFinish(name)
-    g_pools[name]['periodFinish'] = periodFinish
-    console.log(`${name} reward expiration date is initialized to ${periodFinish}  (${formatTimestamp(periodFinish)})`)
-    metadata[name] = `periodFinish: ${periodFinish}  (${formatTimestamp(periodFinish)})`
+    const periodFinish = await readPeriodFinish(poolName)
+    g_pools[poolName]['periodFinish'] = periodFinish
+    console.log(`${poolName} reward expiration date is initialized to ${periodFinish}  (${formatTimestamp(periodFinish)})`)
+    metadata[poolName] = `periodFinish: ${periodFinish}  (${formatTimestamp(periodFinish)})`
   }
 
-  return {}
+  return metadata
 }
 
-async function handlePeriodFinishChange(pool: string, newPeriodFinish: number, findings: Finding[]) {
+async function handlePeriodFinishChange(poolName: string, newPeriodFinish: number, findings: Finding[]) {
     console.log(`Rewards prolonged`)
-    g_pools[pool].periodFinish = newPeriodFinish
-    g_pools[pool].periodExpired = false
+    g_pools[poolName].periodFinish = newPeriodFinish
+    g_pools[poolName].periodExpired = false
 
     findings.push(Finding.fromObject({
-      name: `${pool} rewards prolonged`,
-      description: `${pool} rewards successfully prolonged till ${formatTimestamp(g_pools[pool].periodFinish)}`,
-      alertId: `LDO-${pool.toUpperCase()}-REWARDS-PROLONGED-DEBUG`,
+      name: `${poolName} rewards prolonged`,
+      description: `${poolName} rewards successfully prolonged till ${formatTimestamp(g_pools[poolName].periodFinish)}`,
+      alertId: `LDO-${poolName.toUpperCase()}-REWARDS-PROLONGED-DEBUG`,
       severity: FindingSeverity.Info,
       type: FindingType.Info,
     }))
 }
 
 async function handleRewardExpire(poolName: string, blockEvent: BlockEvent, findings: Finding[]) {
-  const info = g_pools[poolName]
+  let poolInfo = g_pools[poolName]
   const periodFinishUpdated = await readPeriodFinish(poolName)
 
   const handleStillNotProlonged = function () {
@@ -123,11 +125,11 @@ async function handleRewardExpire(poolName: string, blockEvent: BlockEvent, find
       severity: FindingSeverity.Critical,
       type: FindingType.Info,
     }))
-    info.lastNotification = now
+    poolInfo.lastNotification = now
   }
 
   const handlePeriodExpiration = async function () {
-    const sushiRewardsBalance = await ldoToken.balanceOf(info.managerAddress)
+    const sushiRewardsBalance = await ldoToken.balanceOf(poolInfo.managerAddress)
     if (sushiRewardsBalance === 0) {
       findings.push(Finding.fromObject({
         name: `${poolName} rewards expired but no LDO`,
@@ -137,28 +139,30 @@ async function handleRewardExpire(poolName: string, blockEvent: BlockEvent, find
         type: FindingType.Info,
       }))
     }
-    info.periodExpired = true
+    poolInfo.periodExpired = true
   }
 
   const handleBeforehandPeriod = async function(periodParams: any) {
-    const { beforehandPeriod, description, minimumLdo, severity } = periodParams
+    const { period, description, minManagerLdoBalance, severity, pools } = periodParams
+    if (!pools.includes(poolName)) {
+      return
+    }
 
-    const thisPeriodWasAlreadyReported = info.lastNotification >= info.periodFinish - beforehandPeriod
-    if (timeTillEnd < beforehandPeriod && !thisPeriodWasAlreadyReported) {
-      const sushiRewardsBalance = await ldoToken.balanceOf(info.managerAddress)
+    const thisPeriodWasAlreadyReported = poolInfo.lastNotification >= poolInfo.periodFinish - period
+    if (timeTillEnd < period && !thisPeriodWasAlreadyReported) {
+      const poolManagerBalance = await ldoToken.balanceOf(poolInfo.managerAddress)
 
-      if (minimumLdo !== null) {
-        const minLdo = ethers.utils.parseUnits(minimumLdo, LDO_NUM_DECIMALS)
-        if (sushiRewardsBalance.gte(minLdo)) {
+      if (minManagerLdoBalance !== null) {
+        const minLdo = ethers.utils.parseUnits(minManagerLdoBalance, LDO_NUM_DECIMALS)
+        if (poolManagerBalance.gte(minLdo)) {
           return
         }
       }
 
-      info.lastNotification = now
-      
+      poolInfo.lastNotification = now
       findings.push(Finding.fromObject({
         name: `${poolName} rewards period expiriation`,
-        description: description,
+        description: description(poolName),
         alertId: `LDO-${poolName.toUpperCase()}-REWARDS-EXPIRATION-DEBUG`,
         severity: severity,
         type: FindingType.Info,
@@ -166,28 +170,27 @@ async function handleRewardExpire(poolName: string, blockEvent: BlockEvent, find
     }
   }
 
-  if (periodFinishUpdated !== info.periodFinish) {
+  if (periodFinishUpdated !== poolInfo.periodFinish) {
     handlePeriodFinishChange(poolName, periodFinishUpdated, findings)
     return
   }
 
   const now = blockEvent.block.timestamp
-  const timeTillEnd = info.periodFinish - now
+  const timeTillEnd = poolInfo.periodFinish - now
 
-  const stillNotProlongedWasAlreadyReported = info.lastNotification >= info.periodFinish + POOL_REWARDS_STILL_NOT_PROLONGED_ALERT_PERIOD
-  if (timeTillEnd < -POOL_REWARDS_STILL_NOT_PROLONGED_ALERT_PERIOD && !stillNotProlongedWasAlreadyReported) { // 10 min passed and still not prolonged
+  const maxDelay = MAX_DELAY_OF_POOL_REWARDS_PERIOD_PROLONGATION
+  const stillNotProlongedWasAlreadyReported = poolInfo.lastNotification >= poolInfo.periodFinish + maxDelay
+  if (timeTillEnd < -maxDelay && !stillNotProlongedWasAlreadyReported) { // 10 min passed and still not prolonged
     // No need to check time of last RewardAdded event (of rewards contract),
     // because we already know it due to the cached periodFinish has not been updated
     handleStillNotProlonged()
   }
-  else if (timeTillEnd <= 0 && !info.periodExpired) { // period has expired
+  else if (timeTillEnd <= 0 && !poolInfo.periodExpired) { // period has expired
     await handlePeriodExpiration()
   }
   else {
-    const beforehands = info.alerts.sort((x: any, y: any) => { return x.beforehandPeriod - y.beforehandPeriod })
-    for (let i = 0; i < beforehands.length; i++) {
-      console.log(`${poolName} period ${beforehands[i].beforehandPeriod}`)
-      handleBeforehandPeriod(beforehands[i])
+    for (let i = 0; i < POOL_REWARDS_ALERTS_PERIODS_PARAMS.length; i++) {
+      await handleBeforehandPeriod(POOL_REWARDS_ALERTS_PERIODS_PARAMS[i])
     }
   }
 }
