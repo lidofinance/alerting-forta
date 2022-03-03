3edeba5f94bb9b74e2b5c398463b9ec83ddec81e
Dmitry Gusakov <dmitry.gusakov@p2p.org>
2022-03-03 14:02:10 +0300

    Fix EasyTrack alerts

--- src/agent-easy-track.ts
+++ src/agent-easy-track.ts
@@ -35,18 +35,20 @@ export async function handleTransaction(txEvent: TransactionEvent) {
 }
 
 function handleEasyTrackTransaction(txEvent: TransactionEvent, findings: Finding[]) {
-  for (let i = 0; i < EASY_TRACK_EVENTS_OF_NOTICE.length; i++) {
-    const eventInfo = EASY_TRACK_EVENTS_OF_NOTICE[i]
+  EASY_TRACK_EVENTS_OF_NOTICE.forEach(eventInfo => {
     if (txEvent.to === eventInfo.address) {
       const [event] = txEvent.filterLog(eventInfo.event, eventInfo.address)
-      findings.push(Finding.fromObject({
-        name: event.name,
-        description: eventInfo.description(event.args),
-        alertId: eventInfo.alertId,
-        severity: eventInfo.severity,
-        type: FindingType.Info,
-        metadata: event.args,
-      }))
+      if (event) {
+        findings.push(Finding.fromObject({
+          name: event.name,
+          description: eventInfo.description(event.args),
+          alertId: eventInfo.alertId,
+          severity: eventInfo.severity,
+          type: FindingType.Info,
+          metadata: { args: String(event.args) },
+        }))
+      }
     }
   }
+  )
 }
