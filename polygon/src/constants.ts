816e6f98eab02ed6e2f66351b75c589979b84855
Dmitry Gusakov <dmitry.gusakov@p2p.org>
2022-03-10 15:57:40 +0300

    Add a separate polygon agent

---
+++ polygon/src/constants.ts
@@ -0,0 +1,21 @@
+import BigNumber from 'bignumber.js'
+import {
+    FindingSeverity,
+} from 'forta-agent'
+
+// COMMON CONSTS
+export const MATIC_DECIMALS = new BigNumber(10 ** 18)
+
+// 24 hours
+export const FULL_24_HOURS = 24 * 60 * 60
+
+// ADDRESSES
+export const MATIC_TOKEN_ADDRESS = '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0'
+export const ST_MATIC_TOKEN_ADDRESS = '0x9ee91f9f426fa633d227f7a9b000e28b9dfd8599'
+
+// THRESHOLDS
+// 3.1% MATIC of total pooled MATIC
+export const MAX_BUFFERED_MATIC_IMMEDIATE_PERCENT = 25.1
+
+// 1.1% MATIC of total pooled MATIC
+export const MAX_BUFFERED_MATIC_DAILY_PERCENT = 1.1
