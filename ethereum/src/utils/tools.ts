9ba3d41d354dde691db227b466007c8bf5228d1d
Dmitry Gusakov <dmitry.gusakov@p2p.org>
2022-03-14 12:48:27 +0300

    Stability fixes (#30)
    * Separate oracle and beth agents
    
    * Use exact block number in pools balances calls
    
    * Remove debug console.log

--- ethereum/src/utils/tools.ts
+++ ethereum/src/utils/tools.ts
@@ -1,4 +1,7 @@
+import { Event } from "ethers";
+
 export function capitalizeFirstLetter(str: string) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
-  
+
+export const byBlockNumberDesc = (e1: Event, e2: Event) => e2.blockNumber - e1.blockNumber;
