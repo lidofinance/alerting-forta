43213986677b058fe445e0adf4cb71e4c177d012
Dmitry Gusakov <dmitry.gusakov@p2p.org>
2022-05-06 08:26:50 +0300

    feat: Add ARAGON-VOTE-OUTCOME-CHANGED alert (#51)
    Emitted when there is more than 36h passed since the vote start
    and support state changes from 'For' to 'Against' and vice-versa.

---
+++ ethereum/src/agent-aragon-voting.ts
@@ -0,0 +1,207 @@
+import BigNumber from "bignumber.js";
+
+import {
+  ethers,
+  BlockEvent,
+  TransactionEvent,
+  Finding,
+  FindingType,
+  FindingSeverity,
+  Block,
+  TxEventBlock,
+} from "forta-agent";
+
+import { ethersProvider } from "./ethers";
+
+import ARAGON_VOTING_ABI from "./abi/AragonVoting.json";
+import {
+  LIDO_ARAGON_VOTING_ADDRESS,
+  CAST_VOTE_EVENT,
+  ETH_DECIMALS,
+} from "./constants";
+
+export const name = "Aragon Voting Watcher";
+
+// Perform ad-hoc votes info refresh each BLOCK_WINDOW blocks
+const BLOCK_WINDOW = 1000;
+
+// Number of blocks for the whole 5 days
+const FIVE_DAYS_BLOCKS = Math.floor((60 * 60 * 24 * 5) / 13);
+
+// 36 hours
+const HALF_VOTING_TIME = 60 * 60 * 36;
+
+interface IVoteInfo {
+  startDate: number;
+  open: boolean;
+  yea: BigNumber;
+  nay: BigNumber;
+  votingPower: BigNumber;
+  supportRequired: number;
+  minAcceptQuorum: number;
+}
+
+enum Outcomes {
+  Pass = "Pass",
+  Fail = "Fail",
+}
+
+let votes = new Map<number, IVoteInfo>();
+
+export async function initialize(
+  currentBlock: number
+): Promise<{ [key: string]: string }> {
+  console.log(`[${name}]`);
+  const aragonVoting = new ethers.Contract(
+    LIDO_ARAGON_VOTING_ADDRESS,
+    ARAGON_VOTING_ABI,
+    ethersProvider
+  );
+  const filterStartVote = aragonVoting.filters.StartVote();
+  const startedVotes = (
+    await aragonVoting.queryFilter(
+      filterStartVote,
+      currentBlock - FIVE_DAYS_BLOCKS,
+      currentBlock
+    )
+  ).map((value: any) => parseInt(String(value.args.voteId)));
+
+  await Promise.all(
+    startedVotes.map(async (voteId: number) => {
+      const voteInfo = await getVoteInfo(voteId, currentBlock);
+      if (voteInfo.open) {
+        votes.set(voteId, voteInfo);
+      }
+    })
+  );
+  return { activeVotes: `${Array.from(votes.keys())}` };
+}
+
+export async function handleBlock(blockEvent: BlockEvent) {
+  const findings: Finding[] = [];
+
+  await Promise.all([adHocVotesRefresh(blockEvent, findings)]);
+
+  return findings;
+}
+
+async function adHocVotesRefresh(blockEvent: BlockEvent, findings: Finding[]) {
+  if (blockEvent.blockNumber % BLOCK_WINDOW == 0) {
+    await Promise.all(
+      Array.from(votes.keys()).map(async (voteId: number) => {
+        await handleNewVoteInfo(voteId, blockEvent.block, findings);
+      })
+    );
+  }
+}
+
+async function getVoteInfo(
+  voteId: number,
+  blockNumber: number
+): Promise<IVoteInfo> {
+  const aragonVoting = new ethers.Contract(
+    LIDO_ARAGON_VOTING_ADDRESS,
+    ARAGON_VOTING_ABI,
+    ethersProvider
+  );
+  const voteInfoRaw = await aragonVoting.functions.getVote(voteId, {
+    blockTag: blockNumber,
+  });
+  return {
+    startDate: voteInfoRaw.startDate.toNumber(),
+    open: voteInfoRaw.open,
+    yea: new BigNumber(String(voteInfoRaw.yea)),
+    nay: new BigNumber(String(voteInfoRaw.nay)),
+    votingPower: new BigNumber(String(voteInfoRaw.votingPower)),
+    supportRequired: new BigNumber(String(voteInfoRaw.supportRequired))
+      .div(ETH_DECIMALS)
+      .toNumber(),
+    minAcceptQuorum: new BigNumber(String(voteInfoRaw.minAcceptQuorum))
+      .div(ETH_DECIMALS)
+      .toNumber(),
+  };
+}
+
+async function handleNewVoteInfo(
+  voteId: number,
+  block: Block | TxEventBlock,
+  findings: Finding[]
+) {
+  const oldVoteInfo = votes.get(voteId);
+  const newVoteInfo = await getVoteInfo(voteId, block.number);
+  // delete vote form map if vote is closed
+  if (!newVoteInfo.open) {
+    votes.delete(voteId);
+  }
+  if (oldVoteInfo) {
+    const now = block.timestamp;
+    const oldOutcome = getVoteOutcome(oldVoteInfo);
+    const newOutcome = getVoteOutcome(newVoteInfo);
+    if (
+      oldOutcome != newOutcome &&
+      newVoteInfo.startDate + HALF_VOTING_TIME < now
+    ) {
+      findings.push(
+        Finding.fromObject({
+          name: "Expected vote outcome has changed",
+          description:
+            `Expected aragon vote ${voteId} outcome changed from '${oldOutcome}' to '${newOutcome}' and there is more than ` +
+            `${Math.floor(HALF_VOTING_TIME / (60 * 60))} hours ` +
+            `past since vote start`,
+          alertId: "ARAGON-VOTE-OUTCOME-CHANGED",
+          severity: FindingSeverity.High,
+          type: FindingType.Suspicious,
+        })
+      );
+    }
+  }
+  // update vote info
+  votes.set(voteId, newVoteInfo);
+}
+
+function getVoteOutcome(voteInfo: IVoteInfo): Outcomes {
+  // quorum not reached
+  if (
+    voteInfo.yea.isLessThan(
+      voteInfo.votingPower.times(voteInfo.minAcceptQuorum)
+    )
+  ) {
+    return Outcomes.Fail;
+  }
+  // it should be more than 50% yea votes
+  if (
+    voteInfo.yea.isLessThanOrEqualTo(
+      voteInfo.nay.plus(voteInfo.yea).times(voteInfo.supportRequired)
+    )
+  ) {
+    return Outcomes.Fail;
+  }
+  return Outcomes.Pass;
+}
+
+export async function handleTransaction(txEvent: TransactionEvent) {
+  const findings: Finding[] = [];
+
+  await handleEasyTrackTransaction(txEvent, findings);
+
+  return findings;
+}
+
+async function handleEasyTrackTransaction(
+  txEvent: TransactionEvent,
+  findings: Finding[]
+) {
+  if (LIDO_ARAGON_VOTING_ADDRESS in txEvent.addresses) {
+    const [event] = txEvent.filterLog(
+      CAST_VOTE_EVENT,
+      LIDO_ARAGON_VOTING_ADDRESS
+    );
+    if (event && event.args.voteId) {
+      await handleNewVoteInfo(
+        event.args.voteId.toNumber(),
+        txEvent.block,
+        findings
+      );
+    }
+  }
+}
