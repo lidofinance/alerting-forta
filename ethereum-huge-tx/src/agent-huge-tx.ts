f86a16fd3f6ea8471b6c46ce92cb0af39d09b8da
Dmitry Gusakov <dmitry.gusakov@lido.fi>
2022-05-24 17:43:14 +0300

    feat: Special texts for the well known transfers (#86)

--- ethereum-huge-tx/src/agent-huge-tx.ts
+++ ethereum-huge-tx/src/agent-huge-tx.ts
@@ -1,5 +1,3 @@
-import BigNumber from "bignumber.js";
-
 import {
   ethers,
   BlockEvent,
@@ -12,11 +10,12 @@ import {
 
 import {
   MONITORED_TOKENS,
-  ETH_DECIMALS,
   TRANSFER_EVENT,
   TX_AMOUNT_THRESHOLD,
-  ADDRESS_TO_NAME,
-  NULL_ADDRESS,
+  TransferEventInfo,
+  SPECIAL_TRANSFERS,
+  SpecialTransferPattern,
+  PARTIALLY_MONITORED_TOKENS,
 } from "./constants";
 
 export const name = "Huge TX detector";
@@ -36,16 +35,17 @@ export async function handleTransaction(txEvent: TransactionEvent) {
 
 async function handleHugeTx(txEvent: TransactionEvent, findings: Finding[]) {
   let transfersTexts: string[] = [];
-  Object.entries(MONITORED_TOKENS).map(async (keyValue: string[]) => {
-    const [tokenName, tokenAddress] = keyValue;
-    if (tokenAddress in txEvent.addresses) {
-      const transferEvents = txEvent.filterLog(TRANSFER_EVENT, tokenAddress);
-      transferEvents.forEach((event) => {
-        const transferText = prepareTransferEventText(event, tokenName);
-        if (transferText) {
-          transfersTexts.push(transferText);
-        }
-      });
+  const transferEvents = txEvent
+    .filterLog(TRANSFER_EVENT)
+    .filter(
+      (event) =>
+        MONITORED_TOKENS.get(event.address.toLowerCase()) ||
+        PARTIALLY_MONITORED_TOKENS.get(event.address.toLowerCase())
+    );
+  transferEvents.forEach((event) => {
+    const transferText = prepareTransferEventText(event);
+    if (transferText) {
+      transfersTexts.push(transferText);
     }
   });
   if (transfersTexts.length > 0) {
@@ -53,7 +53,8 @@ async function handleHugeTx(txEvent: TransactionEvent, findings: Finding[]) {
       Finding.fromObject({
         name: "Huge token(s) of Lido interest in a single TX",
         description:
-          transfersTexts.join("\n") + `\nhttps://etherscan.io/tx/${txEvent.hash}`,
+          transfersTexts.join("\n") +
+          `\nhttps://etherscan.io/tx/${txEvent.hash}`,
         alertId: "HUGE-TOKEN-TRANSFERS-IN-SINGLE-TX",
         severity: FindingSeverity.Info,
         type: FindingType.Info,
@@ -62,36 +63,47 @@ async function handleHugeTx(txEvent: TransactionEvent, findings: Finding[]) {
   }
 }
 
-function prepareTransferEventText(transferEvent: LogDescription, tokenName: string) {
-  if (transferEvent.args._value) {
-    const amount = new BigNumber(String(transferEvent.args._value)).div(
-      ETH_DECIMALS
-    );
-    if (amount.isGreaterThanOrEqualTo(TX_AMOUNT_THRESHOLD)) {
-      const from = transferEvent.args._from.toLowerCase();
-      const fromName = ADDRESS_TO_NAME.get(from) || "unknown";
-      const to = transferEvent.args._to.toLowerCase();
-      const toName = ADDRESS_TO_NAME.get(to) || "unknown";
-      if (from == NULL_ADDRESS) {
-        return (
-          `**${amount.toFixed(2)} ${tokenName}** ` +
-          `were minted.\n` +
-          `To: ${to} (${toName})`
-        );
+function prepareTransferEventText(transferEvent: LogDescription) {
+  const transferInfo = new TransferEventInfo(transferEvent);
+  if (transferInfo.amount.isGreaterThanOrEqualTo(TX_AMOUNT_THRESHOLD)) {
+    for (const transferPattern of SPECIAL_TRANSFERS) {
+      if (match(transferPattern, transferInfo)) {
+        return transferPattern.description(transferInfo);
       }
-      if (to == NULL_ADDRESS) {
-        return (
-          `**${amount.toFixed(2)} ${tokenName}** ` +
-          `were burned.\n` +
-          `From: ${from} (${fromName})`
-        );
-      }
+    }
+    // Do not report on common transfers of PARTIALLY_MONITORED_TOKENS
+    if (!PARTIALLY_MONITORED_TOKENS.get(transferInfo.token)) {
       return (
-        `**${amount.toFixed(2)} ${tokenName}** ` +
+        `**${transferInfo.amount.toFixed(2)} ${transferInfo.tokenName}** ` +
         `were transferred.\n` +
-        `From: ${from} (${fromName})\n` +
-        `To: ${to} (${toName})`
+        `From: ${transferInfo.from} (${transferInfo.fromName})\n` +
+        `To: ${transferInfo.to} (${transferInfo.toName})`
       );
     }
   }
+}
+
+function match(
+  transferPattern: SpecialTransferPattern,
+  transferInfo: TransferEventInfo
+): boolean {
+  if (
+    transferPattern.contract &&
+    transferInfo.token != transferPattern.contract.toLowerCase()
+  ) {
+    return false;
+  }
+  if (
+    transferPattern.from &&
+    transferInfo.from != transferPattern.from.toLowerCase()
+  ) {
+    return false;
+  }
+  if (
+    transferPattern.to &&
+    transferInfo.to != transferPattern.to.toLowerCase()
+  ) {
+    return false;
+  }
+  return true;
 }
