e6e63962ea4d23d1514e4d18d2dc0580bd3a55ec
Dmitry Gusakov <dmitry.gusakov@lido.fi>
2022-05-25 11:29:15 +0300

    feat: Add 0x: ERC20 Proxy to WL (#90)

--- phishing-detect/src/constants.ts
+++ phishing-detect/src/constants.ts
@@ -102,6 +102,7 @@ const WHITE_LIST_ADDRESSES_RAW: string [] = [
   "0x36c744dd2916e9e04173bee9d93d554f955a999d", // senseWstETHAdapter
   "0x27182842e098f60e3d576794a5bffb0777e025d3", // euler
   "0x78106f7db3ebcee3d2cfac647f0e4c9b06683b39", // dustSweeper
+  "0x95e6f48254609a6ee006f7d493c8e5fb97094cef", // 0x: ERC20 Proxy
 ];
 
 export const WHITE_LIST_ADDRESSES: string[] = WHITE_LIST_ADDRESSES_RAW.map(address => address.toLowerCase())
