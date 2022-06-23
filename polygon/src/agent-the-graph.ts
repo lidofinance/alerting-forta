2f1816793f32be9472b136909daac83823f31066
Dmitry Gusakov <gusakov.dv@gmail.com>
2022-06-23 10:31:06 +0300

    feat: Graph balance report window

--- polygon/src/agent-the-graph.ts
+++ polygon/src/agent-the-graph.ts
@@ -22,6 +22,10 @@ import { ethersProvider } from "./ethers";
 
 export const name = "TheGraph";
 
+// 24 hours
+const REPORT_WINDOW_GRAPH_BALANCE = 60 * 60 * 24;
+let lastReportedGraphBalance = 0;
+
 export async function initialize(
   currentBlock: number
 ): Promise<{ [key: string]: string }> {
@@ -39,29 +43,33 @@ async function handleLidoGraphBalance(
   blockEvent: BlockEvent,
   findings: Finding[]
 ) {
-  const billing = new ethers.Contract(
-    BILLING_ADDRESS,
-    BILLING_ABI,
-    ethersProvider
-  );
-
-  const balance = new BigNumber(
-    String(await billing.functions.userBalances(LIDO_VAULT_ADDRESS))
-  ).div(MATIC_DECIMALS);
-
-  if (balance.isLessThanOrEqualTo(GRAPH_BALANCE_THRESHOLD)) {
-    findings.push(
-      Finding.fromObject({
-        name: "Low balance of Lido account on The Graph",
-        description: `Balance is ${balance.toFixed(2)} GRT. It is too low!`,
-        alertId: "LOW-LIDO-GRAPH-BALANCE",
-        severity: FindingSeverity.High,
-        type: FindingType.Degraded,
-        metadata: {
-          balance: balance.toFixed(2),
-          lido_vault_address: LIDO_VAULT_ADDRESS,
-        },
-      })
+  const now = blockEvent.block.timestamp;
+  if (lastReportedGraphBalance + REPORT_WINDOW_GRAPH_BALANCE < now) {
+    const billing = new ethers.Contract(
+      BILLING_ADDRESS,
+      BILLING_ABI,
+      ethersProvider
     );
+  
+    const balance = new BigNumber(
+      String(await billing.functions.userBalances(LIDO_VAULT_ADDRESS))
+    ).div(MATIC_DECIMALS);
+  
+    if (balance.isLessThanOrEqualTo(GRAPH_BALANCE_THRESHOLD)) {
+      findings.push(
+        Finding.fromObject({
+          name: "Low balance of Lido account on The Graph",
+          description: `Balance is ${balance.toFixed(2)} GRT. It is too low!`,
+          alertId: "LOW-LIDO-GRAPH-BALANCE",
+          severity: FindingSeverity.High,
+          type: FindingType.Degraded,
+          metadata: {
+            balance: balance.toFixed(2),
+            lido_vault_address: LIDO_VAULT_ADDRESS,
+          },
+        })
+      );
+      lastReportedGraphBalance = now
+    }
   }
 }
