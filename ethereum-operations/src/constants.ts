08cafabeb0f77d6d058c7a78cd19aaf5b705f341
Dmitry Gusakov <gusakov.dv@gmail.com>
2022-07-17 13:27:28 +0300

    fix: Format

--- ethereum-operations/src/constants.ts
+++ ethereum-operations/src/constants.ts
@@ -118,7 +118,8 @@ export const ARAGON_VOTING_EVENTS_OF_NOTICE = [
 ];
 
 export const EASY_TRACK_ADDRESS = "0xf0211b7660680b49de1a7e9f25c65660f0a13fea";
-export const MOTION_ENACTED_EVENT = "event MotionEnacted(uint256 indexed _motionId)";
+export const MOTION_ENACTED_EVENT =
+  "event MotionEnacted(uint256 indexed _motionId)";
 export const EVM_SCRIPT_EXECUTOR_ADDRESS =
   "0xfe5986e06210ac1ecc1adcafc0cc7f8d63b3f977";
 export const REWARD_PROGRAMS_REGISTRY_ADDRESS =
