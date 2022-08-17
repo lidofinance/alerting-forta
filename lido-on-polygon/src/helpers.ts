7faf136bd51490184b82e256e73ebd5f2ef8a0e9
Dmitry Gusakov <gusakov.dv@gmail.com>
2022-08-17 09:05:53 +0300

    chore: Format numbers + add missing suffixes

---
+++ lido-on-polygon/src/helpers.ts
@@ -0,0 +1,19 @@
+const SI_SYMBOL = ['', 'k', 'M', 'G', 'T', 'P', 'E']
+
+export function abbreviateNumber(number: number): string {
+    // what tier? (determines SI symbol)
+    const tier = (Math.log10(Math.abs(number)) / 3) | 0
+
+    // if zero, we don't need a suffix
+    if (tier == 0) return Math.round(number).toString()
+
+    // get suffix and determine scale
+    const suffix = SI_SYMBOL[tier]
+    const scale = Math.pow(10, tier * 3)
+
+    // scale the number
+    const scaled = number / scale
+
+    // format number and add suffix
+    return scaled.toFixed(1) + suffix
+}
