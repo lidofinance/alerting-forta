b11dcf8fdd3bee97b19e7bef45a4f5826fd551b1
Dmitry Gusakov <gusakov.dv@gmail.com>
2022-10-05 16:07:05 +0400

    fix: USDC address

--- ethereum-operations/src/constants.ts
+++ ethereum-operations/src/constants.ts
@@ -50,10 +50,7 @@ export const KNOWN_ERC20 = new Map<string, ERC20>([
   ["0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32", { decimals: 18, name: "LDO" }],
   ["0x6B175474E89094C44Da98b954EedeAC495271d0F", { decimals: 18, name: "DAI" }],
   ["0xdAC17F958D2ee523a2206206994597C13D831ec7", { decimals: 6, name: "USDT" }],
-  [
-    "0xb9e31a22e3a1c743c6720f3b723923e91f3c0f8b",
-    { decimals: 18, name: "USDC" },
-  ],
+  ["0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", { decimals: 6, name: "USDC" }],
 ]);
 
 export const NODE_OPERATORS_REGISTRY_EVENTS_OF_NOTICE = [
