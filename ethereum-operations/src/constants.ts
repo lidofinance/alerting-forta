75b1f1cb5a52ad37bee0f0438feb496beaa959b5
Dmitry Gusakov <gusakov.dv@gmail.com>
2022-10-05 10:06:44 +0400

    chore: Add token address

--- ethereum-operations/src/constants.ts
+++ ethereum-operations/src/constants.ts
@@ -639,7 +639,7 @@ export const INSURANCE_FUND_EVENTS_OF_NOTICE = [
       };
       return `${new BigNumber(String(args._amount))
         .div(10 ** tokenInfo.decimals)
-        .toFixed(2)} of ${tokenInfo.name} were transferred from insurance fund to ${
+        .toFixed(2)} of ${args._token}(${tokenInfo.name}) were transferred from insurance fund to ${
         args._recipient
       }`;
     },
