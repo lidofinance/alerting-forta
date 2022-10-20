d4760981b82b333e61755f7b6454f05900c9c6e5
Dmitry Gusakov <gusakov.dv@gmail.com>
2022-10-20 16:05:21 +0400

    chore: Add emoji to l2-bridge-arbitrum bot

--- l2-bridge-arbitrum/src/constants.ts
+++ l2-bridge-arbitrum/src/constants.ts
@@ -11,23 +11,23 @@ export const ETH_DECIMALS = new BigNumber(10).pow(18);
 export const ROLES = new Map<string, string>([
   [
     "0x63f736f21cb2943826cd50b191eb054ebbea670e4e962d0527611f830cd399d6",
-    "DEPOSITS_DISABLER_ROLE",
+    "DEPOSITS DISABLER ROLE",
   ],
   [
     "0x4b43b36766bde12c5e9cbbc37d15f8d1f769f08f54720ab370faeb4ce893753a",
-    "DEPOSITS_ENABLER_ROLE",
+    "DEPOSITS ENABLER ROLE",
   ],
   [
     "0x94a954c0bc99227eddbc0715a62a7e1056ed8784cd719c2303b685683908857c",
-    "WITHDRAWALS_DISABLER_ROLE",
+    "WITHDRAWALS DISABLER ROLE",
   ],
   [
     "0x9ab8816a3dc0b3849ec1ac00483f6ec815b07eee2fd766a353311c823ad59d0d",
-    "WITHDRAWALS_ENABLER_ROLE",
+    "WITHDRAWALS ENABLER ROLE",
   ],
   [
     "0x0000000000000000000000000000000000000000000000000000000000000000",
-    "DEFAULT_ADMIN_ROLE",
+    "DEFAULT ADMIN ROLE",
   ],
 ]);
 
@@ -64,7 +64,7 @@ export const GOV_BRIDGE_EVENTS: EventOfNotice[] = [
     event:
       "event EthereumGovernanceExecutorUpdate(address oldEthereumGovernanceExecutor, address newEthereumGovernanceExecutor)",
     alertId: "GOV-BRIDGE-EXEC-UPDATED",
-    name: "Arbitrum Gov Bridge: Ethereum Governance Executor Updated",
+    name: "🚨 Arbitrum Gov Bridge: Ethereum Governance Executor Updated",
     description: (args: any) =>
       `Ethereum Governance Executor was updated from ` +
       `${args.oldEthereumGovernanceExecutor} to ${args.newEthereumGovernanceExecutor}`,
@@ -75,7 +75,7 @@ export const GOV_BRIDGE_EVENTS: EventOfNotice[] = [
     address: GOV_BRIDGE_ADDRESS,
     event: "event GuardianUpdate(address oldGuardian, address newGuardian)",
     alertId: "GOV-BRIDGE-GUARDIAN-UPDATED",
-    name: "Arbitrum Gov Bridge: Guardian Updated",
+    name: "🚨 Arbitrum Gov Bridge: Guardian Updated",
     description: (args: any) =>
       `Ethereum Governance Guardian was updated from ` +
       `${args.oldGuardian} to ${args.newGuardian}`,
@@ -86,7 +86,7 @@ export const GOV_BRIDGE_EVENTS: EventOfNotice[] = [
     address: GOV_BRIDGE_ADDRESS,
     event: "event DelayUpdate(uint256 oldDelay, uint256 newDelay)",
     alertId: "GOV-BRIDGE-DELAY-UPDATED",
-    name: "Arbitrum Gov Bridge: Delay Updated",
+    name: "⚠️ Arbitrum Gov Bridge: Delay Updated",
     description: (args: any) =>
       `Delay was updated from ` + `${args.oldDelay} to ${args.newDelay}`,
     severity: FindingSeverity.High,
@@ -97,7 +97,7 @@ export const GOV_BRIDGE_EVENTS: EventOfNotice[] = [
     event:
       "event GracePeriodUpdate(uint256 oldGracePeriod, uint256 newGracePeriod)",
     alertId: "GOV-BRIDGE-GRACE-PERIOD-UPDATED",
-    name: "Arbitrum Gov Bridge: Grace Period Updated",
+    name: "⚠️ Arbitrum Gov Bridge: Grace Period Updated",
     description: (args: any) =>
       `Grace Period was updated from ` +
       `${args.oldGracePeriod} to ${args.newGracePeriod}`,
@@ -109,7 +109,7 @@ export const GOV_BRIDGE_EVENTS: EventOfNotice[] = [
     event:
       "event MinimumDelayUpdate(uint256 oldMinimumDelay, uint256 newMinimumDelay)",
     alertId: "GOV-BRIDGE-MIN-DELAY-UPDATED",
-    name: "Arbitrum Gov Bridge: Min Delay Updated",
+    name: "⚠️ Arbitrum Gov Bridge: Min Delay Updated",
     description: (args: any) =>
       `Min Delay was updated from ` +
       `${args.oldMinimumDelay} to ${args.newMinimumDelay}`,
@@ -121,7 +121,7 @@ export const GOV_BRIDGE_EVENTS: EventOfNotice[] = [
     event:
       "event MaximumDelayUpdate(uint256 oldMaximumDelay, uint256 newMaximumDelay)",
     alertId: "GOV-BRIDGE-MAX-DELAY-UPDATED",
-    name: "Arbitrum Gov Bridge: Max Delay Updated",
+    name: "⚠️ Arbitrum Gov Bridge: Max Delay Updated",
     description: (args: any) =>
       `Max Delay was updated from ` +
       `${args.oldMaximumDelay} to ${args.newMaximumDelay}`,
@@ -133,7 +133,7 @@ export const GOV_BRIDGE_EVENTS: EventOfNotice[] = [
     event:
       "event ActionsSetQueued(uint256 indexed id, address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, bool[] withDelegatecalls, uint256 executionTime)",
     alertId: "GOV-BRIDGE-ACTION-SET-QUEUED",
-    name: "Arbitrum Gov Bridge: Action set queued",
+    name: "ℹ Arbitrum Gov Bridge: Action set queued",
     description: (args: any) => `Action set ${args.id} was queued`,
     severity: FindingSeverity.Info,
     type: FindingType.Info,
@@ -143,7 +143,7 @@ export const GOV_BRIDGE_EVENTS: EventOfNotice[] = [
     event:
       "event ActionsSetExecuted(uint256 indexed id, address indexed initiatorExecution, bytes[] returnedData)",
     alertId: "GOV-BRIDGE-ACTION-SET-EXECUTED",
-    name: "Arbitrum Gov Bridge: Action set executed",
+    name: "ℹ Arbitrum Gov Bridge: Action set executed",
     description: (args: any) => `Action set ${args.id} was executed`,
     severity: FindingSeverity.Info,
     type: FindingType.Info,
@@ -152,7 +152,7 @@ export const GOV_BRIDGE_EVENTS: EventOfNotice[] = [
     address: GOV_BRIDGE_ADDRESS,
     event: "event ActionsSetCanceled(uint256 indexed id)",
     alertId: "GOV-BRIDGE-ACTION-SET-CANCELED",
-    name: "Arbitrum Gov Bridge: Action set canceled",
+    name: "❌ Arbitrum Gov Bridge: Action set canceled",
     description: (args: any) => `Action set ${args.id} was canceled`,
     severity: FindingSeverity.Info,
     type: FindingType.Info,
@@ -194,7 +194,7 @@ export const PROXY_ADMIN_EVENTS: EventOfNotice[] = LIDO_PROXY_CONTRACTS.map(
         address: proxyInfo.address,
         event: "event ProxyOssified()",
         alertId: "PROXY-OSSIFIED",
-        name: "Arbitrum: Proxy ossified",
+        name: "🚨 Arbitrum: Proxy ossified",
         description: (args: any) =>
           `Proxy for ${proxyInfo.name}(${proxyInfo.address}) was ossified` +
           `\n(detected by event)`,
@@ -205,7 +205,7 @@ export const PROXY_ADMIN_EVENTS: EventOfNotice[] = LIDO_PROXY_CONTRACTS.map(
         address: proxyInfo.address,
         event: "event AdminChanged(address previousAdmin, address newAdmin)",
         alertId: "PROXY-ADMIN-CHANGED",
-        name: "Arbitrum: Proxy admin changed",
+        name: "🚨 Arbitrum: Proxy admin changed",
         description: (args: any) =>
           `Proxy admin for ${proxyInfo.name}(${proxyInfo.address}) ` +
           `was changed from ${args.previousAdmin} to ${args.newAdmin}` +
@@ -217,7 +217,7 @@ export const PROXY_ADMIN_EVENTS: EventOfNotice[] = LIDO_PROXY_CONTRACTS.map(
         address: proxyInfo.address,
         event: "event Upgraded(address indexed implementation)",
         alertId: "PROXY-UPGRADED",
-        name: "Arbitrum: Proxy upgraded",
+        name: "🚨 Arbitrum: Proxy upgraded",
         description: (args: any) =>
           `Proxy for ${proxyInfo.name}(${proxyInfo.address}) ` +
           `was updated to ${args.implementation}` +
@@ -229,7 +229,7 @@ export const PROXY_ADMIN_EVENTS: EventOfNotice[] = LIDO_PROXY_CONTRACTS.map(
         address: proxyInfo.address,
         event: "event BeaconUpgraded(address indexed beacon)",
         alertId: "PROXY-BEACON-UPGRADED",
-        name: "Arbitrum: Proxy beacon upgraded",
+        name: "🚨 Arbitrum: Proxy beacon upgraded",
         description: (args: any) =>
           `Proxy for ${proxyInfo.name}(${proxyInfo.address}) ` +
           `beacon was updated to ${args.beacon}` +
@@ -248,7 +248,7 @@ export const L2_BRIDGE_EVENTS: EventOfNotice[] = [
     event:
       "event RoleAdminChanged(bytes32 indexed role, bytes32 indexed previousAdminRole, bytes32 indexed newAdminRole)",
     alertId: "L2-BRIDGE-ROLE-ADMIN-CHANGED",
-    name: "Arbitrum L2 Bridge: Role Admin changed",
+    name: "🚨 Arbitrum L2 Bridge: Role Admin changed",
     description: (args: any) =>
       `Role Admin for role ${args.role}(${
         ROLES.get(args.role) || "unknown"
@@ -262,7 +262,7 @@ export const L2_BRIDGE_EVENTS: EventOfNotice[] = [
     event:
       "event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender)",
     alertId: "L2-BRIDGE-ROLE-GRANTED",
-    name: "Arbitrum L2 Bridge: Role granted",
+    name: "⚠️ Arbitrum L2 Bridge: Role granted",
     description: (args: any) =>
       `Role ${args.role}(${ROLES.get(args.role) || "unknown"}) ` +
       `was granted to ${args.account} by ${args.sender}`,
@@ -274,7 +274,7 @@ export const L2_BRIDGE_EVENTS: EventOfNotice[] = [
     event:
       "event RoleRevoked(bytes32 indexed role, address indexed account, address indexed sender)",
     alertId: "L2-BRIDGE-ROLE-REVOKED",
-    name: "Arbitrum L2 Bridge: Role revoked",
+    name: "⚠️ Arbitrum L2 Bridge: Role revoked",
     description: (args: any) =>
       `Role ${args.role}(${ROLES.get(args.role) || "unknown"}) ` +
       `was revoked to ${args.account} by ${args.sender}`,
@@ -285,7 +285,7 @@ export const L2_BRIDGE_EVENTS: EventOfNotice[] = [
     address: L2_ERC20_TOKEN_GATEWAY,
     event: "event DepositsEnabled(address indexed enabler)",
     alertId: "L2-BRIDGE-DEPOSITS-ENABLED",
-    name: "Arbitrum L2 Bridge: Deposits Enabled",
+    name: "✅ Arbitrum L2 Bridge: Deposits Enabled",
     description: (args: any) => `Deposits were enabled by ${args.enabler}`,
     severity: FindingSeverity.High,
     type: FindingType.Info,
@@ -294,7 +294,7 @@ export const L2_BRIDGE_EVENTS: EventOfNotice[] = [
     address: L2_ERC20_TOKEN_GATEWAY,
     event: "event DepositsDisabled(address indexed disabler)",
     alertId: "L2-BRIDGE-DEPOSITS-DISABLED",
-    name: "Arbitrum L2 Bridge: Deposits Disabled",
+    name: "❌ Arbitrum L2 Bridge: Deposits Disabled",
     description: (args: any) => `Deposits were disabled by ${args.disabler}`,
     severity: FindingSeverity.High,
     type: FindingType.Info,
@@ -303,7 +303,7 @@ export const L2_BRIDGE_EVENTS: EventOfNotice[] = [
     address: L2_ERC20_TOKEN_GATEWAY,
     event: "event WithdrawalsEnabled(address indexed enabler)",
     alertId: "L2-BRIDGE-WITHDRAWALS-ENABLED",
-    name: "Arbitrum L2 Bridge: Withdrawals Enabled",
+    name: "✅ Arbitrum L2 Bridge: Withdrawals Enabled",
     description: (args: any) => `Withdrawals were enabled by ${args.enabler}`,
     severity: FindingSeverity.High,
     type: FindingType.Info,
@@ -312,7 +312,7 @@ export const L2_BRIDGE_EVENTS: EventOfNotice[] = [
     address: L2_ERC20_TOKEN_GATEWAY,
     event: "event WithdrawalsDisabled(address indexed disabler)",
     alertId: "L2-BRIDGE-WITHDRAWALS-DISABLED",
-    name: "Arbitrum L2 Bridge: Withdrawals Disabled",
+    name: "❌ Arbitrum L2 Bridge: Withdrawals Disabled",
     description: (args: any) => `Withdrawals were disabled by ${args.enabler}`,
     severity: FindingSeverity.High,
     type: FindingType.Info,
@@ -321,7 +321,7 @@ export const L2_BRIDGE_EVENTS: EventOfNotice[] = [
     address: L2_ERC20_TOKEN_GATEWAY,
     event: "event Initialized(address indexed admin)",
     alertId: "L2-BRIDGE-IMPLEMENTATION-INITIALIZED",
-    name: "Arbitrum L2 Bridge: Implementation initialized",
+    name: "🚨 Arbitrum L2 Bridge: Implementation initialized",
     description: (args: any) =>
       `Implementation of the Arbitrum L2 Bridge was initialized by ${args.admin}\n` +
       `NOTE: This is not the thing that should be left unacted! ` +
