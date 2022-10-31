969323d71ffb4058928ee1f1a9d8c21ed5d939e5
Dmitry Gusakov <gusakov.dv@gmail.com>
2022-11-01 00:35:59 +0400

    feat: Storage watcher bot

---
+++ storage-watcher/src/agent-storage-watcher.ts
@@ -0,0 +1,78 @@
+import BigNumber from "bignumber.js";
+
+import {
+  ethers,
+  BlockEvent,
+  TransactionEvent,
+  Finding,
+  FindingType,
+  FindingSeverity,
+} from "forta-agent";
+
+import { STORAGE_SLOTS, ContractStorageMap, StorageSlot } from "./constants";
+import { getStorageValue } from "./helpers";
+
+export const name = "DaoOps";
+
+let contractsStorageValues: Map<string, Map<string, string>> = new Map();
+
+export async function initialize(
+  currentBlock: number
+): Promise<{ [key: string]: string }> {
+  console.log(`[${name}]`);
+  const findings: Finding[] = [];
+  await handleStorageSlots(currentBlock, findings, false);
+  return {};
+}
+
+export async function handleBlock(blockEvent: BlockEvent) {
+  const findings: Finding[] = [];
+
+  await handleStorageSlots(blockEvent.blockNumber, findings, true);
+
+  return findings;
+}
+
+async function handleStorageSlots(
+  blockNumber: number,
+  findings: Finding[],
+  checkValues: boolean
+) {
+  await Promise.all(
+    STORAGE_SLOTS.map(async (contractStorageMap: ContractStorageMap) => {
+      const contract = contractStorageMap.contract;
+      let contractInfo =
+        contractsStorageValues.get(contract.address) ||
+        new Map<string, string>();
+      await Promise.all(
+        contractStorageMap.slots.map(async (slot: StorageSlot) => {
+          const value = await getStorageValue(
+            contractStorageMap.contract.address,
+            slot,
+            blockNumber
+          );
+          if (checkValues) {
+            const prevValue = contractInfo.get(slot.name);
+            if (value != prevValue && prevValue) {
+              findings.push(
+                Finding.fromObject({
+                  name: `🚨 Critical storage slot value changed`,
+                  description:
+                    `Value of the storage slot ${slot.name} ` +
+                    `for contract ${contract.address} (${contract.name}) has changed!` +
+                    `\nPrev value: ${prevValue}` +
+                    `\nNew value: ${value}`,
+                  alertId: "STORAGE-SLOT-VALUE-CHANGED",
+                  severity: FindingSeverity.High,
+                  type: FindingType.Suspicious,
+                })
+              );
+            }
+          }
+          contractInfo.set(slot.name, value);
+        })
+      );
+      contractsStorageValues.set(contract.address, contractInfo);
+    })
+  );
+}
---
+++ storage-watcher/src/constants.ts
@@ -0,0 +1,171 @@
+import BigNumber from "bignumber.js";
+
+// INTERFACES
+
+export interface StorageSlot {
+  name: string;
+  address?: string;
+}
+
+export interface Contract {
+  name: string;
+  address: string;
+}
+
+export interface ContractStorageMap {
+  contract: Contract;
+  slots: StorageSlot[];
+}
+
+// COMMON CONSTS
+
+// 1 ETH
+export const ETH_DECIMALS = new BigNumber(10).pow(18);
+
+// ADDRESSES AND EVENTS
+
+export const LIDO_ADDRESS = "0xae7ab96520de3a18e5e111b5eaab095312d7fe84";
+export const SELFOWNEDSTETHBURNER_ADDRESS = "0xb280e33812c0b09353180e92e27b8ad399b07f26";
+export const NOR_ADDRESS = "0x55032650b14df07b85bf18a3a3ec8e0af2e028d5";
+export const LIDO_ORACLE_ADDRESS = "0x442af784a788a5bd6f42a01ebe9f287a871243fb";
+export const DEPOSIT_SECURITY_MODULE = "0x710b3303fb508a84f10793c1106e32be873c24cd";
+export const WSTETH_ADDRESS = "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0";
+export const MEV_BOOST_RELAY_ALLOWED_LIST_ADDRESS = "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0";
+export const ARAGON_VOTING_ADDRESS = "0x2e59A20f205bB85a89C53f1936454680651E618e";
+export const ARAGON_TOKEN_MANAGER_ADDRESS = "0xf73a1260d222f447210581ddf212d915c09a3249";
+export const ARAGON_FINANCE_ADDRESS = "0xb9e5cbb9ca5b0d659238807e84d0176930753d86";
+export const LIDO_TREASURY_ADDRESS = "0x3e40d73eb977dc6a537af587d48316fee66e9c8c";
+
+export const SUBMITTED_EVENT =
+  "event Submitted(address indexed sender, uint256 amount, address referral)";
+
+// Storage slots mapping
+
+export const STORAGE_SLOTS: ContractStorageMap[] = [
+  {
+    contract: {
+      name: "Lido and stETH token",
+      address: LIDO_ADDRESS
+    },
+    slots: [
+      { name: "lido.Lido.fee" },
+      { name: "lido.Lido.treasuryFee" },
+      { name: "lido.Lido.insuranceFee" },
+      { name: "lido.Lido.nodeOperatorsFee" },
+      { name: "lido.Lido.treasuryFee" },
+      { name: "lido.Lido.insuranceFund" },
+      { name: "lido.Lido.oracle" },
+      { name: "lido.Lido.depositContract" },
+      { name: "lido.Lido.executionLayerRewardsVault" },
+      { name: "lido.Lido.ELRewardsWithdrawalLimit" },
+      { name: "lido.Lido.withdrawalCredentials" },
+    ],
+  },
+  {
+    contract: {
+      name: "SelfOwnedStETHBurner",
+      address: SELFOWNEDSTETHBURNER_ADDRESS
+    },
+    slots: [
+      { name: "maxBurnAmountPerRunBasisPoints", address: "0x4" },
+    ],
+  },
+  {
+    contract: {
+      name: "Node Operators Registry",
+      address: NOR_ADDRESS
+    },
+    slots: [
+      { name: "lido.NodeOperatorsRegistry.lido" },
+    ],
+  },
+  {
+    contract: {
+      name: "Lido Oracle",
+      address: LIDO_ORACLE_ADDRESS
+    },
+    slots: [
+      { name: "lido.LidoOracle.quorum" },
+      { name: "lido.LidoOracle.lido" },
+      { name: "lido.LidoOracle.beaconSpec" },
+      { name: "lido.LidoOracle.contractVersion" },
+      { name: "lido.LidoOracle.beaconReportReceiver" },
+      { name: "lido.LidoOracle.allowedBeaconBalanceAnnualRelativeIncrease" },
+      { name: "lido.LidoOracle.allowedBeaconBalanceDecrease" },
+    ],
+  },
+  {
+    contract: {
+      name: "Lido Oracle",
+      address: DEPOSIT_SECURITY_MODULE
+    },
+    slots: [
+      { name: "nodeOperatorsRegistry", address: "0x0" },
+      { name: "maxDepositsPerBlock", address: "0x1" },
+      { name: "minDepositBlockDistance", address: "0x2" },
+      { name: "pauseIntentValidityPeriodBlocks", address: "0x3" },
+      { name: "owner", address: "0x4" },
+      { name: "quorum", address: "0x7" },
+      { name: "paused", address: "0x8" },
+    ],
+  },
+  {
+    contract: {
+      name: "wstETH",
+      address: WSTETH_ADDRESS
+    },
+    slots: [
+      { name: "stETH", address: "0x7" },
+    ],
+  },
+  {
+    contract: {
+      name: "MEV Boost Relay Allowed List",
+      address: MEV_BOOST_RELAY_ALLOWED_LIST_ADDRESS
+    },
+    slots: [
+      { name: "owner", address: "0x0" },
+      { name: "manager", address: "0x1" },
+      { name: "allowed_list_version", address: "0x4003" },
+    ],
+  },
+  {
+    contract: {
+      name: "Aragon Voting",
+      address: ARAGON_VOTING_ADDRESS
+    },
+    slots: [
+      { name: "token", address: "0x0" },
+      { name: "supportRequiredPct, minAcceptQuorumPct, voteTime", address: "0x1" },
+      { name: "objectionPhaseTime", address: "0x4" },
+    ],
+  },
+  {
+    contract: {
+      name: "Aragon Token Manager",
+      address: ARAGON_TOKEN_MANAGER_ADDRESS
+    },
+    slots: [
+      { name: "token", address: "0x0" },
+      { name: "maxAccountTokens", address: "0x1" },
+    ],
+  },
+  {
+    contract: {
+      name: "Aragon Finance",
+      address: ARAGON_FINANCE_ADDRESS
+    },
+    slots: [
+      { name: "vault", address: "0x0" },
+    ],
+  },
+  {
+    contract: {
+      name: "Lido Treasury",
+      address: LIDO_TREASURY_ADDRESS
+    },
+    slots: [
+      { name: "designatedSigner", address: "0x1" },
+    ],
+  },
+];
