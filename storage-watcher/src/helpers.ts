2f7eeb392673630d3fd94904a13c9338a01583bb
Dmitry Gusakov <gusakov.dv@gmail.com>
2022-11-03 13:30:51 +0400

    chore: Linter

--- storage-watcher/src/helpers.ts
+++ storage-watcher/src/helpers.ts
@@ -3,7 +3,17 @@ import { ethersProvider } from "./ethers";
 import { keccak256 } from "@ethersproject/keccak256";
 import { toUtf8Bytes } from "@ethersproject/strings";
 
-export async function getStorageValue(address: string, slot: StorageSlot, block?: number) {
-    const slotAddress = slot.address ? slot.address : keccak256(toUtf8Bytes(slot.name))
-    return await ethersProvider.getStorageAt(address, slotAddress, block ? block : "latest")
+export async function getStorageValue(
+  address: string,
+  slot: StorageSlot,
+  block?: number
+) {
+  const slotAddress = slot.address
+    ? slot.address
+    : keccak256(toUtf8Bytes(slot.name));
+  return await ethersProvider.getStorageAt(
+    address,
+    slotAddress,
+    block ? block : "latest"
+  );
 }
