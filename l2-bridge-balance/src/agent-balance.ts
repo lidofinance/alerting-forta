78a2a17548194bba0a4980acd6cc3f82abc7bb49
Dmitry Gusakov <dmitry.gusakov@lido.fi>
2022-11-04 11:56:13 +0400

    feat: LDO to L2 bridges alerts (#187)

--- l2-bridge-balance/src/agent-balance.ts
+++ l2-bridge-balance/src/agent-balance.ts
@@ -8,10 +8,13 @@ import {
   FindingSeverity,
 } from "forta-agent";
 import {
-  BRIDGE_PARAMS,
+  BRIDGE_PARAMS_WSTETH,
+  BRIDGE_PARAMS_LDO,
   WSTETH_ADDRESS,
   ETH_DECIMALS,
-  BridgeParam,
+  BridgeParamWstETH,
+  BridgeParamLDO,
+  LDO_ADDRESS,
 } from "./constants";
 import ERC20_SHORT_ABI from "./abi/ERC20Short.json";
 import { ethersProvider } from "./ethers";
@@ -29,17 +32,27 @@ export async function handleBlock(blockEvent: BlockEvent) {
   const findings: Finding[] = [];
 
   await Promise.all([
-    handleBridgeBalance(blockEvent, findings, BRIDGE_PARAMS.Arbitrum),
-    handleBridgeBalance(blockEvent, findings, BRIDGE_PARAMS.Optimism),
+    handleBridgeBalanceWstETH(
+      blockEvent,
+      findings,
+      BRIDGE_PARAMS_WSTETH.Arbitrum
+    ),
+    handleBridgeBalanceWstETH(
+      blockEvent,
+      findings,
+      BRIDGE_PARAMS_WSTETH.Optimism
+    ),
+    handleBridgeBalanceLDO(blockEvent, findings, BRIDGE_PARAMS_LDO.Arbitrum),
+    handleBridgeBalanceLDO(blockEvent, findings, BRIDGE_PARAMS_LDO.Optimism),
   ]);
 
   return findings;
 }
 
-async function handleBridgeBalance(
+async function handleBridgeBalanceWstETH(
   blockEvent: BlockEvent,
   findings: Finding[],
-  networkParams: BridgeParam
+  networkParams: BridgeParamWstETH
 ) {
   const wstETH = new ethers.Contract(
     WSTETH_ADDRESS,
@@ -74,6 +87,47 @@ async function handleBridgeBalance(
           l1Balance: l1Balance.toFixed(),
           l1Address: networkParams.l1Gateway,
           l2Token: networkParams.wstEthBridged,
+          network: networkParams.name,
+        },
+      })
+    );
+  }
+}
+
+async function handleBridgeBalanceLDO(
+  blockEvent: BlockEvent,
+  findings: Finding[],
+  networkParams: BridgeParamLDO
+) {
+  const LDO = new ethers.Contract(LDO_ADDRESS, ERC20_SHORT_ABI, ethersProvider);
+  const l1Balance = new BigNumber(
+    String(await LDO.functions.balanceOf(networkParams.l1Gateway))
+  );
+  const l2Provider = new ethers.providers.JsonRpcProvider(networkParams.rpcUrl);
+  const bridgedLDO = new ethers.Contract(
+    networkParams.ldoBridged,
+    ERC20_SHORT_ABI,
+    l2Provider
+  );
+  const l2TotalSupply = new BigNumber(
+    String(await bridgedLDO.functions.totalSupply())
+  );
+  if (l2TotalSupply.isGreaterThan(l1Balance)) {
+    findings.push(
+      Finding.fromObject({
+        name: `🚨🚨🚨 ${networkParams.name} bridge balance mismatch 🚨🚨🚨`,
+        description:
+          `Total supply of bridged LDO is greater than balanceOf L1 bridge side!\n` +
+          `L2 total supply: ${l2TotalSupply.div(ETH_DECIMALS).toFixed(2)}\n` +
+          `L1 balanceOf: ${l1Balance.div(ETH_DECIMALS).toFixed(2)}\n`,
+        alertId: "BRIDGE-BALANCE-MISMATCH",
+        severity: FindingSeverity.Critical,
+        type: FindingType.Suspicious,
+        metadata: {
+          l2TotalSupply: l2TotalSupply.toFixed(),
+          l1Balance: l1Balance.toFixed(),
+          l1Address: networkParams.l1Gateway,
+          l2Token: networkParams.ldoBridged,
           network: networkParams.name,
         },
       })
--- l2-bridge-balance/src/constants.ts
+++ l2-bridge-balance/src/constants.ts
@@ -10,37 +10,58 @@ export const ETH_DECIMALS = new BigNumber(10).pow(18);
 
 export const WSTETH_ADDRESS = "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0";
 
-export interface BridgeParam {
+export interface BridgeParamWstETH {
   name: string;
-  l2Gateway: string;
   l1Gateway: string;
   wstEthBridged: string;
   rpcUrl: string;
 }
-export interface BridgeParams {
-  Arbitrum: BridgeParam;
-  Optimism: BridgeParam;
+
+export interface BridgeParamsWstETH {
+  Arbitrum: BridgeParamWstETH;
+  Optimism: BridgeParamWstETH;
 }
 
-export const BRIDGE_PARAMS: BridgeParams = {
+export const BRIDGE_PARAMS_WSTETH: BridgeParamsWstETH = {
   Arbitrum: {
     name: "Arbitrum",
-    l2Gateway: "0x07d4692291b9e30e326fd31706f686f83f331b82",
     l1Gateway: "0x0f25c1dc2a9922304f2eac71dca9b07e310e8e5a",
     wstEthBridged: "0x5979d7b546e38e414f7e9822514be443a4800529",
     rpcUrl: config.Arbitrum.RpcUrl,
   },
   Optimism: {
     name: "Optimism",
-    l2Gateway: "0x8e01013243a96601a86eb3153f0d9fa4fbfb6957",
     l1Gateway: "0x76943c0d61395d8f2edf9060e1533529cae05de6",
     wstEthBridged: "0x1f32b1c2345538c0c6f582fcb022739c4a194ebb",
     rpcUrl: config.Optimism.RpcUrl,
   },
 };
 
-export const WITHDRAWAL_INITIATED_EVENT =
-  "event WithdrawalInitiated(address l1Token, address indexed from, address indexed to, uint256 indexed l2ToL1Id, uint256 exitNum, uint256 amount)";
+export const LDO_ADDRESS = "0x5a98fcbea516cf06857215779fd812ca3bef1b32";
 
-export const WITHDRAWAL_FINALIZED_EVENT =
-  "event WithdrawalFinalized(address l1Token, address indexed from, address indexed to, uint256 indexed exitNum, uint256 amount)";
+export interface BridgeParamLDO {
+  name: string;
+  l1Gateway: string;
+  ldoBridged: string;
+  rpcUrl: string;
+}
+
+export interface BridgeParamsLDO {
+  Arbitrum: BridgeParamLDO;
+  Optimism: BridgeParamLDO;
+}
+
+export const BRIDGE_PARAMS_LDO: BridgeParamsLDO = {
+  Arbitrum: {
+    name: "Arbitrum",
+    l1Gateway: "0xa3a7b6f88361f48403514059f1f16c8e78d60eec",
+    ldoBridged: "0x13ad51ed4f1b7e9dc168d8a00cb3f4ddd85efa60",
+    rpcUrl: config.Arbitrum.RpcUrl,
+  },
+  Optimism: {
+    name: "Optimism",
+    l1Gateway: "0x99c9fc46f92e8a1c0dec1b1747d010903e884be1",
+    ldoBridged: "0xfdb794692724153d1488ccdbe0c56c252596735f",
+    rpcUrl: config.Optimism.RpcUrl,
+  },
+};
