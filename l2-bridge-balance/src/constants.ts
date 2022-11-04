60895e65061710ea230c3a9e9d336e04b336f765
Dmitry Gusakov <gusakov.dv@gmail.com>
2022-11-04 11:26:50 +0400

    chore: Remove debug stuff

--- l2-bridge-balance/src/constants.ts
+++ l2-bridge-balance/src/constants.ts
@@ -12,7 +12,6 @@ export const WSTETH_ADDRESS = "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0";
 
 export interface BridgeParamWstETH {
   name: string;
-  //  l2Gateway: string;
   l1Gateway: string;
   wstEthBridged: string;
   rpcUrl: string;
@@ -26,14 +25,12 @@ export interface BridgeParamsWstETH {
 export const BRIDGE_PARAMS_WSTETH: BridgeParamsWstETH = {
   Arbitrum: {
     name: "Arbitrum",
-    // l2Gateway: "0x07d4692291b9e30e326fd31706f686f83f331b82",
     l1Gateway: "0x0f25c1dc2a9922304f2eac71dca9b07e310e8e5a",
     wstEthBridged: "0x5979d7b546e38e414f7e9822514be443a4800529",
     rpcUrl: config.Arbitrum.RpcUrl,
   },
   Optimism: {
     name: "Optimism",
-    // l2Gateway: "0x8e01013243a96601a86eb3153f0d9fa4fbfb6957",
     l1Gateway: "0x76943c0d61395d8f2edf9060e1533529cae05de6",
     wstEthBridged: "0x1f32b1c2345538c0c6f582fcb022739c4a194ebb",
     rpcUrl: config.Optimism.RpcUrl,
