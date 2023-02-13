8fa94ce8c30849371d7c6d29d300a6ba710bb8d4
madlabman <10616301+madlabman@users.noreply.github.com>
2023-02-13 17:05:46 +0100

    chore: add SY-wstETH-WETH BalancerLP Aura

--- phishing-detect/src/constants.ts
+++ phishing-detect/src/constants.ts
@@ -159,6 +159,7 @@ const WHITE_LIST_ADDRESSES_RAW: string[] = [
   "0x87870bca3f3fd6335c3f4ce8392d69350b4fa4e2", // AAVE v3
   "0xb748952c7bc638f31775245964707bcc5ddfabfc", // AAVE v3 Migration Helper
   "0xd98e1c56a56a0ec5ca4bf6fdfbfa1572ee4d8a8d", // maverick-swap-alpha
+  "0x8267fdabd1b8c8645138f2de5b0fe24988dc9820", // SY-wstETH-WETH BalancerLP Aura
 ];
 
 export const WHITE_LIST_ADDRESSES: string[] = WHITE_LIST_ADDRESSES_RAW.map(
