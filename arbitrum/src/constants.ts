ead11fd56f7deffa08dad25f016fd1dd2ce854a5
Dmitry Gusakov <dmitry.gusakov@lido.fi>
2023-03-01 13:54:32 +0100

    fix: Increase the graph interval (#248)
    * fix: Increase the graph interval
    
    * chore: use const

--- arbitrum/src/constants.ts
+++ arbitrum/src/constants.ts
@@ -3,6 +3,7 @@ import BigNumber from "bignumber.js";
 // COMMON CONSTS
 export const ETH_DECIMALS = new BigNumber(10).pow(18); // 1 ETH
 export const GRAPH_BALANCE_THRESHOLD = 1000; // 1000 GRT
+export const BLOCK_INTERVAL = 1000; // 1000 blocks
 
 // ADDRESSES AND EVENTS
 export const BILLING_ADDRESS = "0x1B07D3344188908Fb6DEcEac381f3eE63C48477a";
