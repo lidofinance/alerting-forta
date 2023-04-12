979764a8edc16bccd3dd672fed3c70cfa7528acf
vgorkavenko <vladimir.g@lido.fi>
2023-04-12 16:00:41 +0500

    fix: prettier

--- ethereum-operations/src/subagents/easy-track/utils.ts
+++ ethereum-operations/src/subagents/easy-track/utils.ts
@@ -1,7 +1,8 @@
-export const getMotionType = (types: Map<string, string>, evmScriptFactory: string) => {
-  return (
-    types.get(evmScriptFactory.toLowerCase()) || "New "
-  );
+export const getMotionType = (
+  types: Map<string, string>,
+  evmScriptFactory: string
+) => {
+  return types.get(evmScriptFactory.toLowerCase()) || "New ";
 };
 
 export const getMotionLink = (motionId: string) => {
