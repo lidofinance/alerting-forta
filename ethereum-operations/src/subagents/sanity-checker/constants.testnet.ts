2516a352801d6630c1637ba7172ba99f7036cd54
vgorkavenko <vladimir.g@lido.fi>
2023-04-17 14:37:54 +0500

    fix: use constants from mainnet

--- ethereum-operations/src/subagents/sanity-checker/constants.testnet.ts
+++ ethereum-operations/src/subagents/sanity-checker/constants.testnet.ts
@@ -1,14 +1,7 @@
+import * as mainnet from "./constants";
+
 export const ORACLE_REPORT_SANITY_CHECKER_ADDRESS =
   "0x0950C6cbe9D8D11432a4e2d99232690E8254f35f";
 
-export const ORACLE_REPORT_SANITY_CHECKER_LIMITS = [
-  "churnValidatorsPerDayLimit",
-  "oneOffCLBalanceDecreaseBPLimit",
-  "annualBalanceIncreaseBPLimit",
-  "simulatedShareRateDeviationBPLimit",
-  "maxValidatorExitRequestsPerReport",
-  "maxAccountingExtraDataListItemsCount",
-  "maxNodeOperatorsPerExtraDataItemCount",
-  "requestTimestampMargin",
-  "maxPositiveTokenRebase",
-];
+export const ORACLE_REPORT_SANITY_CHECKER_LIMITS =
+  mainnet.ORACLE_REPORT_SANITY_CHECKER_LIMITS;
