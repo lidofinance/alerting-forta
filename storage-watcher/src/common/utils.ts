1ca0cfe59c918991f0c93c37d7f59d866dec9437
madlabman <10616301+madlabman@users.noreply.github.com>
2023-04-18 14:50:12 +0200

    feat: storage-watcher on Lido v2

--- storage-watcher/src/common/utils.ts
+++ storage-watcher/src/common/utils.ts
@@ -2,18 +2,40 @@ import { StorageSlot } from "../subagents/storage-watcher/constants";
 import { ethersProvider } from "../ethers";
 import { keccak256 } from "@ethersproject/keccak256";
 import { toUtf8Bytes } from "@ethersproject/strings";
+import BigNumber from "bignumber.js";
 
 export async function getStorageValue(
   address: string,
   slot: StorageSlot,
   block?: number
 ) {
+  const blockId = block ? block : "latest";
   const slotAddress = slot.address
     ? slot.address
     : keccak256(toUtf8Bytes(slot.name));
-  return await ethersProvider.getStorageAt(
-    address,
-    slotAddress,
-    block ? block : "latest"
-  );
+
+  if (slot.isArray) {
+    const len = BigNumber(
+      await ethersProvider.getStorageAt(address, slotAddress, blockId)
+    ).toNumber();
+
+    const arrayStart =
+      "0x" + BigNumber(slotAddress, 16).toString(16).padStart(64, "0");
+
+    const values = [];
+
+    for (let i = 0; i < len; i++) {
+      values.push(
+        await ethersProvider.getStorageAt(
+          address,
+          "0x" + BigNumber(keccak256(arrayStart)).plus(i).toString(16),
+          blockId
+        )
+      );
+    }
+
+    return values;
+  }
+
+  return await ethersProvider.getStorageAt(address, slotAddress, blockId);
 }
