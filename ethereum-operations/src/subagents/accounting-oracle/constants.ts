52125537d5a583cf42f3d89625c1f78d65e0b339
Vladimir Gorkavenko <32727352+vgorkavenko@users.noreply.github.com>
2023-05-01 11:57:07 +0400

    chore: oracle addresses (#279)
    * chore: oracle addresses
    
    * fix: case

--- ethereum-operations/src/subagents/accounting-oracle/constants.ts
+++ ethereum-operations/src/subagents/accounting-oracle/constants.ts
@@ -16,16 +16,15 @@ export const ACCOUNTING_HASH_CONSENSUS_REPORT_RECEIVED_EVENT =
   "event ReportReceived(uint256 indexed refSlot, address indexed member, bytes32 report)";
 
 export const ACCOUNTING_ORACLE_MEMBERS = new Map<string, string>([
-  // todo: should be renamed to right names
-  ["0x19b1bebe4773fec2496fef8b81a9c175a823844b", "Chorus One"],
-  ["0x7eE534a6081d57AFB25b5Cff627d4D26217BB0E9", "Chorus One"],
-  ["0x4c75FA734a39f3a21C57e583c1c29942F021C6B7", "Staking Facilities"],
-  ["0xA8aF49FB44AAA8EECa9Ae918bb7c05e2E71c9DE9", "P2P Validator"],
-  ["0x3799bDA7B884D33F79CEC926af21160dc47fbe05", "Stakefish"],
-  ["0x1a13648EE85386cC101d2D7762e2848372068Bc3", "Rated"],
-  ["0xb29dD2f6672C0DFF2d2f173087739A42877A5172", "bloXroute"],
-  ["0xfdA7E01B2718C511bF016030010572e833C7aE6A", "Instadapp"],
-  ["0x81E411f1BFDa43493D7994F82fb61A415F6b8Fd4", "Kyber Network"],
+  ["0x140bd8fbdc884f48da7cb1c09be8a2fadfea776e", "Chorus One"],
+  ["0x1d0813bf088be3047d827d98524fbf779bc25f00", "jumpcrypto"],
+  ["0x404335bce530400a5814375e7ec1fb55faff3ea2", "Staking Facilities"],
+  ["0x007de4a5f7bc37e2f26c0cb2e8a95006ee9b89b5", "P2P Validator"],
+  ["0x946d3b081ed19173dc83cd974fc69e1e760b7d78", "Stakefish"],
+  ["0xec4bfbaf681eb505b94e4a7849877dc6c600ca3a", "Rated"],
+  ["0x61c91ecd902eb56e314bb2d5c5c07785444ea1c8", "bloXroute"],
+  ["0x1ca0fec59b86f549e1f1184d97cb47794c8af58d", "Instadapp"],
+  ["0xa7410857abbf75043d61ea54e07d57a6eb6ef186", "Kyber Network"],
 ]);
 
 export const MAX_REPORT_SUBMIT_SKIP_BLOCKS_INFO = Math.floor(
