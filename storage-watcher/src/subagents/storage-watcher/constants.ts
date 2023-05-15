4794ee415eadc09e8bca1b135e9aa6cc60c5c310
Don Perignom <10616301+madlabman@users.noreply.github.com>
2023-05-15 10:35:06 +0200

    fix: remove stakeLimit slot (#304)

--- storage-watcher/src/subagents/storage-watcher/constants.ts
+++ storage-watcher/src/subagents/storage-watcher/constants.ts
@@ -46,7 +46,6 @@ export const STORAGE_SLOTS: ContractStorageMap[] = [
     slots: [
       { name: "lido.Versioned.contractVersion" },
       { name: "lido.Lido.lidoLocator" },
-      { name: "lido.Lido.stakeLimit" },
     ],
   },
   {
