ab429844bd7f0861f35d51a19b01c5ce5d384ee2
madlabman <10616301+madlabman@users.noreply.github.com>
2023-06-05 12:44:58 +0200

    fix(ethereum-financial): remove Curve LDO rewards

--- ethereum-financial/src/subagents/pools-rewards/constants.ts
+++ ethereum-financial/src/subagents/pools-rewards/constants.ts
@@ -6,13 +6,13 @@ export const LDO_TOKEN_ADDRESS = "0x5a98fcbea516cf06857215779fd812ca3bef1b32";
 export const MAX_DELAY_OF_POOL_REWARDS_PERIOD_PROLONGATION = 10 * 60; // 10 mins
 
 // rewardsAddress is needed only if manager contract doesn't have `period_finish` function
-export const POOLS_PARAMS = {
-  Curve: {
-    managerAddress: "0x753d5167c31fbeb5b49624314d74a957eb271709",
-    rewardsAddress: "0x99ac10631f69c753ddb595d074422a0922d9056b",
-    poolContractAddress: "0xdc24316b9ae028f1497c275eb9192a3ea0f67022",
-  },
-};
+export const POOLS_PARAMS: {
+  [key: string]: {
+    managerAddress: string;
+    rewardsAddress: string;
+    poolContractAddress: string;
+  };
+} = {};
 
 const period10days = 10 * 24 * 60 * 60;
 const period5days = 5 * 24 * 60 * 60;
@@ -27,7 +27,7 @@ export const POOL_REWARDS_ALERTS_PERIODS_PARAMS = [
     description: (poolName: string) =>
       `${poolName} rewards period expires in 2 days and LDO balance is under 10,000 LDO`,
     severity: FindingSeverity.High,
-    pools: ["Curve"],
+    pools: [],
   },
   {
     period: period3days,
@@ -35,7 +35,7 @@ export const POOL_REWARDS_ALERTS_PERIODS_PARAMS = [
     description: (poolName: string) =>
       `${poolName} rewards period expires in 3 days`,
     severity: FindingSeverity.High,
-    pools: ["Curve"],
+    pools: [],
   },
   {
     period: period5days,
@@ -43,7 +43,7 @@ export const POOL_REWARDS_ALERTS_PERIODS_PARAMS = [
     description: (poolName: string) =>
       `${poolName} rewards period expires in 5 days`,
     severity: FindingSeverity.Info,
-    pools: ["Curve"],
+    pools: [],
   },
   {
     period: period10days,
@@ -51,6 +51,6 @@ export const POOL_REWARDS_ALERTS_PERIODS_PARAMS = [
     description: (poolName: string) =>
       `${poolName} rewards period expires in 10 days`,
     severity: FindingSeverity.Info,
-    pools: ["Curve"],
+    pools: [],
   },
 ];
