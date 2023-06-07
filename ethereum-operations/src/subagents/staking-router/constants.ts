dab2f99c28c85db4bfcb71251cef46054619ddd6
madlabman <10616301+madlabman@users.noreply.github.com>
2023-06-07 12:11:30 +0200

    fix(ethereum-operations): lower severity for staking-router warnings

--- ethereum-operations/src/subagents/staking-router/constants.ts
+++ ethereum-operations/src/subagents/staking-router/constants.ts
@@ -31,9 +31,9 @@ export const STAKING_ROUTER_EVENTS_OF_NOTICE = [
     event:
       "event ExitedAndStuckValidatorsCountsUpdateFailed(uint256 indexed stakingModuleId, bytes lowLevelRevertData)",
     alertId: "STAKING-ROUTER-EXITED-AND-STUCK-UPDATE-FAILED",
-    name: "🚨 Staking Router: exited and stuck validators counts update failed",
+    name: "⚠️ Staking Router: exited and stuck validators counts update failed",
     description: (args: any) => `Staking module ID: ${args.stakingModuleId}`,
-    severity: FindingSeverity.Critical,
+    severity: FindingSeverity.High,
   },
   {
     address: STAKING_ROUTER_ADDRESS,
@@ -60,12 +60,12 @@ export const STAKING_ROUTER_EVENTS_OF_NOTICE = [
     event:
       "event StakingModuleStatusSet(uint256 indexed stakingModuleId, StakingModuleStatus status, address setBy)",
     alertId: "STAKING-ROUTER-MODULE-STATUS-SET",
-    name: "🚨 Staking Router: staking module status set",
+    name: "⚠️ Staking Router: staking module status set",
     description: (args: any) =>
       `ID: ${args.stakingModuleId}\nStatus: ${
         args.status
       }\nSet by: ${etherscanAddress(args.setBy)}`,
-    severity: FindingSeverity.Critical,
+    severity: FindingSeverity.High,
   },
   {
     address: STAKING_ROUTER_ADDRESS,
