5d1401701a7de3f3b22f2baee7fc2cce260fcd58
Dmitry Gusakov <gusakov.dv@gmail.com>
2023-07-12 19:40:03 +0400

    feat: Enrich Lido Digest metadata

--- ethereum-operations/src/subagents/lido-report/utils.ts
+++ ethereum-operations/src/subagents/lido-report/utils.ts
@@ -21,3 +21,10 @@ export function formatDelay(fullDelaySec: number) {
     `${delaySec} sec`
   );
 }
+
+export const formatBN2Str = (bn: BigNumber): string => {
+  return bn.toNumber().toLocaleString("en-US", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+};
