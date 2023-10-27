ce04dba3eece13fbdee263308e09b80695b07918
Dmitry Gusakov <dmitry.gusakov@lido.fi>
2023-10-27 12:05:24 +0400

    fix: WL address (#430)

--- phishing-detect/src/constants.ts
+++ phishing-detect/src/constants.ts
@@ -240,7 +240,7 @@ const WHITE_LIST_ADDRESSES_RAW: string[] = [
   "0x1ebfd36223079dc79fefc62260db9e25f3f5e2c7", // Strike ERC20 delegator
   "0x16d104009964e694761c0bf09d7be49b7e3c26fd", // Ipor Router
   "0xf9ab542616a0c8fa94e41c968622c3b2367f5ad1", // Range Uniswap WETH/WSTETH 0.01% LP
-  "0x03f7f3b8da875881206655d8099b9dacf721f1ef", // Enzyme ComptrollerLib
+  "0x20273e8c0d83e449ac1d18fea7f56ff3c749d1e0", // Enzyme ComptrollerLib
   "0xbaa87546cf87b5de1b0b52353a86792d40b8ba70", // Astrid Finance
 ];
 
