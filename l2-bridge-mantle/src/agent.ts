4b2f6988e704d5ba481d91d087b4937342fff7cb
Sergey White <sergey.i@lido.fi>
2023-12-13 16:06:06 +0300

    feat: l2-bridge-mantle add mutex for writing findings from proxyWatcher. cs fix

--- l2-bridge-mantle/src/agent.ts
+++ l2-bridge-mantle/src/agent.ts
@@ -122,7 +122,6 @@ export const handleBlock = (initFinding: Finding[]): HandleBlock => {
       ...bridgeEventFindings,
       ...govEventFindings,
       ...proxyAdminEventFindings,
-      //...proxyWatcherFindings,
       ...monitorWithdrawalsFindings,
     )
 
