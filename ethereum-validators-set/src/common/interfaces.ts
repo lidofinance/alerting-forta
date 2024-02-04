b20ff97da88379dd424e927b79a99bf9cb6ce2c8
Alexander Movsunov <alex.mov@lido.fi>
2024-02-04 14:58:17 +0400

    common interfaces

--- ethereum-validators-set/src/common/interfaces.ts
+++ ethereum-validators-set/src/common/interfaces.ts
@@ -1,27 +1,78 @@
-import { Finding, FindingSeverity, TransactionEvent } from "forta-agent";
+import { Finding, TransactionEvent, ethers } from "forta-agent";
 
-export interface EventsOfNotice {
-  event: string;
-  alertId: string;
-  name: string;
-  description: (...args: any[]) => string;
-  severity: FindingSeverity;
-}
-
-interface EventHandler {
+export interface ContractMethodOrEventHandler<PARAMS> {
   initialize(currentBlock: number): Promise<{ [key: string]: string }>;
   handleTransaction(
     txEvent: TransactionEvent,
-    eventInfo: EventsOfNotice,
+    contract: ethers.Contract,
+    additionalParams: PARAMS,
   ): Promise<Finding[]>;
 }
 
-interface SubagentHandler {
-  registerEventHandler(eventHandler: EventHandler): Promise<void>;
+export abstract class BaseContractHandler<PARAMS> {
+  protected readonly eventHandlers: ContractMethodOrEventHandler<PARAMS>[] = [];
+
+  constructor(
+    protected readonly contract: ethers.Contract,
+    protected readonly contractParams: PARAMS,
+  ) {}
+
+  abstract initialize(currentBlock: number): Promise<{ [key: string]: string }>;
+
+  public addEventHandler(eventHandler: ContractMethodOrEventHandler<PARAMS>) {
+    this.eventHandlers.push(eventHandler);
+  }
+
+  public async handleTransaction(
+    txEvent: TransactionEvent,
+  ): Promise<Finding[]> {
+    if (!(this.contract.address in txEvent.addresses)) {
+      return [];
+    }
+
+    const findings: Finding[][] = [];
+
+    for (const eventHandler of this.eventHandlers) {
+      findings.push(
+        await eventHandler.handleTransaction(
+          txEvent,
+          this.contract,
+          this.contractParams,
+        ),
+      );
+    }
+
+    return findings.flat();
+  }
 }
 
-export class NodeOperatorRegistryHandler implements SubagentHandler {
-  registerEventHandler(eventHandler: EventHandler): Promise<void> {
-    throw new Error("Method not implemented.");
+export class ContractGroupHandler<PARAMS> {
+  constructor(
+    public readonly groupName: string,
+    public readonly contractHandlers: BaseContractHandler<PARAMS>[] = [],
+  ) {}
+
+  public async initialize(
+    currentBlock: number,
+  ): Promise<{ [key: string]: string }> {
+    console.log(`[${this.groupName}]`);
+
+    for (const contractHandler of this.contractHandlers) {
+      await contractHandler.initialize(currentBlock);
+    }
+
+    return {};
+  }
+
+  public async handleTransaction(
+    txEvent: TransactionEvent,
+  ): Promise<Finding[]> {
+    const findings: Finding[][] = [];
+
+    for (const contractHandler of this.contractHandlers) {
+      findings.push(await contractHandler.handleTransaction(txEvent));
+    }
+
+    return findings.flat();
   }
 }
