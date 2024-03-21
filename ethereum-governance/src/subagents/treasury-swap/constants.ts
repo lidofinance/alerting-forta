045ec49886c904de0718dd3bc0f8c9407afa447d
Roman Kolpakov <rkolpakov94@gmail.com>
2024-03-21 22:10:20 +0700

    wip: order creation interception

--- ethereum-governance/src/subagents/treasury-swap/constants.ts
+++ ethereum-governance/src/subagents/treasury-swap/constants.ts
@@ -2,11 +2,25 @@ import { FindingSeverity } from "forta-agent";
 import { etherscanAddress } from "../../common/utils";
 import {
   STONKS_ADDRESS as stonksAddress,
-  ORDER_ADDRESS as orderAddress
+  ORDER_ADDRESS as orderAddress,
 } from "../../common/constants";
 
-export const STONKS_ADDRESS = stonksAddress
-export const ORDER_ADDRESS = orderAddress
+export const STONKS_ADDRESS = stonksAddress;
+export const ORDER_ADDRESS = orderAddress;
+
+export const STONKS_ORDER_CREATION = [
+  {
+    address: stonksAddress.toLocaleLowerCase(),
+    event:
+      "event OrderContractCreated(address indexed orderContract,uint256 minBuyAmount)",
+    alertId: "STONKS-ORDER-CREATED",
+    name: "ℹ️ Stonks: order created",
+    description: (args: any, extraData: any) => {
+      return "Order contract created";
+    },
+    severity: FindingSeverity.Info,
+  },
+];
 
 export const TREASURY_SWAP_EVENTS_OF_NOTICE = [
   {
@@ -16,15 +30,16 @@ export const TREASURY_SWAP_EVENTS_OF_NOTICE = [
     name: "🚨 STONKS Factory: Manager changed",
     description: (args: any) =>
       `Manager of the STONKS factory was changed to ${etherscanAddress(
-        args.manager,
+        args.manager
       )}`,
     severity: FindingSeverity.Critical,
   },
   {
     address: stonksAddress,
-    event: 'event ERC20Recovered(address indexed token, address indexed recipient, uint256 amount)',
-    alertId: 'STONKS-ERC20-RECOVERED',
-    name: 'ℹ️ Order: ERC20 recovered',
+    event:
+      "event ERC20Recovered(address indexed token, address indexed recipient, uint256 amount)",
+    alertId: "STONKS-ERC20-RECOVERED",
+    name: "ℹ️ Order: ERC20 recovered",
     description: (args: any) =>
       `ERC20 recovered:\n` +
       `Requested by: ${etherscanAddress(args.recipient)}\n` +
@@ -34,9 +49,10 @@ export const TREASURY_SWAP_EVENTS_OF_NOTICE = [
   },
   {
     address: stonksAddress,
-    event: 'event ERC721Recovered(address indexed token, uint256 tokenId, address indexed recipient)',
-    alertId: 'STONKS-ERC721-RECOVERED',
-    name: 'ℹ️ Order: ERC721 recovered',
+    event:
+      "event ERC721Recovered(address indexed token, uint256 tokenId, address indexed recipient)",
+    alertId: "STONKS-ERC721-RECOVERED",
+    name: "ℹ️ Order: ERC721 recovered",
     description: (args: any) =>
       `ERC721 recovered:\n` +
       `Requested by: ${etherscanAddress(args.recipient)}\n` +
@@ -46,9 +62,10 @@ export const TREASURY_SWAP_EVENTS_OF_NOTICE = [
   },
   {
     address: stonksAddress,
-    event: 'event ERC1155Recovered(address token, uint256 tokenId, address recipient, uint256 amount)',
-    alertId: 'STONKS-ERC1155-RECOVERED',
-    name: 'ℹ️ Order: ERC1155 recovered',
+    event:
+      "event ERC1155Recovered(address token, uint256 tokenId, address recipient, uint256 amount)",
+    alertId: "STONKS-ERC1155-RECOVERED",
+    name: "ℹ️ Order: ERC1155 recovered",
     description: (args: any) =>
       `ERC1155 recovered:\n` +
       `Requested by: ${etherscanAddress(args.recipient)}\n` +
@@ -59,13 +76,21 @@ export const TREASURY_SWAP_EVENTS_OF_NOTICE = [
   },
   {
     address: stonksAddress,
-    event: 'event EtherRecovered(address indexed recipient, uint256 amount)',
-    alertId: 'STONKS-ETHER-RECOVERED',
-    name: 'ℹ️ Order: Ether recovered',
+    event: "event EtherRecovered(address indexed recipient, uint256 amount)",
+    alertId: "STONKS-ETHER-RECOVERED",
+    name: "ℹ️ Order: Ether recovered",
     description: (args: any) =>
       `Ether recovered:\n` +
       `Requested by: ${etherscanAddress(args.recipient)}\n` +
       `Amount: ${args.amount}`,
+    severity: FindingSeverity.Info,
+  },
+  {
+    address: stonksAddress,
+    event:
+      "event OrderContractCreated(address indexed orderContract,uint256 minBuyAmount)",
+    alertId: "STONKS-ORDER-CREATED",
+    name: "ℹ️ Stonks: order created",
     severity: FindingSeverity.Info,
   },
   {
@@ -75,15 +100,16 @@ export const TREASURY_SWAP_EVENTS_OF_NOTICE = [
     name: "🚨 ORDER Factory: Manager changed",
     description: (args: any) =>
       `Manager of the ORDER factory was changed to ${etherscanAddress(
-        args.manager,
+        args.manager
       )}`,
     severity: FindingSeverity.Critical,
   },
   {
     address: orderAddress,
-    event: 'event ERC20Recovered(address indexed token, address indexed recipient, uint256 amount)',
-    alertId: 'ORDER-ERC20-RECOVERED',
-    name: 'ℹ️ Order: ERC20 recovered',
+    event:
+      "event ERC20Recovered(address indexed token, address indexed recipient, uint256 amount)",
+    alertId: "ORDER-ERC20-RECOVERED",
+    name: "ℹ️ Order: ERC20 recovered",
     description: (args: any) =>
       `ERC20 recovered:\n` +
       `Requested by: ${etherscanAddress(args.recipient)}\n` +
@@ -93,9 +119,10 @@ export const TREASURY_SWAP_EVENTS_OF_NOTICE = [
   },
   {
     address: orderAddress,
-    event: 'event ERC721Recovered(address indexed token, uint256 tokenId, address indexed recipient)',
-    alertId: 'ORDER-ERC721-RECOVERED',
-    name: 'ℹ️ Order: ERC721 recovered',
+    event:
+      "event ERC721Recovered(address indexed token, uint256 tokenId, address indexed recipient)",
+    alertId: "ORDER-ERC721-RECOVERED",
+    name: "ℹ️ Order: ERC721 recovered",
     description: (args: any) =>
       `ERC721 recovered:\n` +
       `Requested by: ${etherscanAddress(args.recipient)}\n` +
@@ -105,9 +132,10 @@ export const TREASURY_SWAP_EVENTS_OF_NOTICE = [
   },
   {
     address: orderAddress,
-    event: 'event ERC1155Recovered(address token, uint256 tokenId, address recipient, uint256 amount)',
-    alertId: 'ORDER-ERC1155-RECOVERED',
-    name: 'ℹ️ Order: ERC1155 recovered',
+    event:
+      "event ERC1155Recovered(address token, uint256 tokenId, address recipient, uint256 amount)",
+    alertId: "ORDER-ERC1155-RECOVERED",
+    name: "ℹ️ Order: ERC1155 recovered",
     description: (args: any) =>
       `ERC1155 recovered:\n` +
       `Requested by: ${etherscanAddress(args.recipient)}\n` +
@@ -118,9 +146,9 @@ export const TREASURY_SWAP_EVENTS_OF_NOTICE = [
   },
   {
     address: orderAddress,
-    event: 'event EtherRecovered(address indexed recipient, uint256 amount)',
-    alertId: 'ORDER-ETHER-RECOVERED',
-    name: 'ℹ️ Order: Ether recovered',
+    event: "event EtherRecovered(address indexed recipient, uint256 amount)",
+    alertId: "ORDER-ETHER-RECOVERED",
+    name: "ℹ️ Order: Ether recovered",
     description: (args: any) =>
       `Ether recovered:\n` +
       `Requested by: ${etherscanAddress(args.recipient)}\n` +
@@ -128,3 +156,24 @@ export const TREASURY_SWAP_EVENTS_OF_NOTICE = [
     severity: FindingSeverity.Info,
   },
 ];
+
+export const createOrderWatchEvent = (
+  address: string,
+  timestamp: Date
+) => {
+  return {
+    address,
+    timestamp,
+    event: {
+      address: address,
+      event:
+        "event Trade(address indexed owner, IERC20 sellToken, IERC20 buyToken, uint256 sellAmount, uint256 buyAmount, uint256 feeAmount, bytes orderUid)",
+      alertId: "STONKS-ORDER-SETTLED",
+      name: "✅ Stonks: Order settled",
+      description: (args: any) => {
+        return "settled";
+      },
+      severity: FindingSeverity.Info,
+    },
+  };
+};
--- ethereum-governance/src/subagents/treasury-swap/treasury-swap.ts
+++ ethereum-governance/src/subagents/treasury-swap/treasury-swap.ts
@@ -1,7 +1,4 @@
-import {
-  Finding,
-  TransactionEvent,
-} from "forta-agent";
+import { Finding, TransactionEvent } from "forta-agent";
 
 import {
   handleEventsOfNotice,
@@ -11,25 +8,40 @@ import {
 import type * as Constants from "./constants";
 
 export const name = "Stonks";
+const createdOrders: any = [];
 
 const {
+  STONKS_ORDER_CREATION,
   TREASURY_SWAP_EVENTS_OF_NOTICE,
+  createOrderWatchEvent,
 } = requireWithTier<typeof Constants>(
   module,
   "./constants",
-  RedefineMode.Merge,
+  RedefineMode.Merge
 );
 
 export async function initialize(
-  currentBlock: number,
+  currentBlock: number
 ): Promise<{ [key: string]: string }> {
-  console.log(`[${name}]`);
   return {};
 }
 
 export async function handleTransaction(txEvent: TransactionEvent) {
+  const orderFindings: Finding[] = [];
   const findings: Finding[] = [];
+
+  handleEventsOfNotice(txEvent, orderFindings, STONKS_ORDER_CREATION);
   handleEventsOfNotice(txEvent, findings, TREASURY_SWAP_EVENTS_OF_NOTICE);
+
+  if (orderFindings.length > 0) {
+    orderFindings.map((event) => {
+      createdOrders.push(
+        createOrderWatchEvent(event.metadata.args.split(",")[0], event.timestamp)
+      );
+      return event.metadata.args.split(",")[0];
+    });
+  }
+
   return findings;
 }
 
