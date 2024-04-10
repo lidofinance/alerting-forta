81feff347b467e9f7bb2f6e0034e99a378cd3cbf
Sergey White <sergey.i@lido.fi>
2024-04-10 15:18:27 +0300

    fix: remove unuseful comments in l2-bridge-linea

--- l2-bridge-linea/src/clients/eth_provider.spec.ts
+++ l2-bridge-linea/src/clients/eth_provider.spec.ts
@@ -1,19 +1,9 @@
 import { App } from '../app'
 import * as E from 'fp-ts/Either'
 import { Address, ETH_DECIMALS } from '../utils/constants'
-// import { JsonRpcProvider } from '@ethersproject/providers'
-// import { ethers } from 'forta-agent'
 import BigNumber from 'bignumber.js'
 
 describe('eth provider tests', () => {
-  // let ethProvider: JsonRpcProvider
-  // const mainnet = 1
-  // const drpcProvider = 'https://eth.drpc.org/'
-
-  beforeAll(async () => {
-    // ethProvider = new ethers.providers.JsonRpcProvider(drpcProvider, mainnet)
-  })
-
   test('getBalanceByBlockHash is 1774.48511061073977627 wsETH', async () => {
     const app = await App.getInstance()
     const adr = Address
