0239d8e8891559a9e62829dccbffdc895d697baa
Ivan Feofanov <feofanov@gmail.com>
2024-04-12 15:04:53 +0200

    fix: removed unused mocks

--- ethereum-governance-v2/src/services/easy-track/mocks/mock.ts
+++
@@ -1,5 +0,0 @@
-import { IEasyTrackClient } from '../contract'
-
-export const EasyTrackMock = (): jest.Mocked<IEasyTrackClient> => ({
-  getNOInfoByMotionData: jest.fn(),
-})
--- ethereum-governance-v2/src/services/proxy-watcher/mocks/mock.ts
+++
@@ -1,6 +0,0 @@
-import { IProxyWatcherClient } from '../contract'
-
-export const ProxyWatcherClientMock = (): jest.Mocked<IProxyWatcherClient> => ({
-  isDeployed: jest.fn(),
-  getProxyImplementation: jest.fn(),
-})
