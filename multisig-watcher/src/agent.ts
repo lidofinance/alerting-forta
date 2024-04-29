997a37df345858af72fcf3f8020350ea6ffeede6
Ivan Feofanov <feofanov@gmail.com>
2024-04-29 14:01:34 +0200

    fix: raise multisig-watcher timeout to 5 minutes due tu timeout errors

--- multisig-watcher/src/agent.ts
+++ multisig-watcher/src/agent.ts
@@ -41,7 +41,7 @@ const subAgents: SubAgent[] = [
 
 // block or tx handling should take no more than 240 sec. Increased due to free RPC usage.
 // If not all processing is done it interrupts the execution, sends current findings and errors as findings too
-const processingTimeout = 240_000;
+const processingTimeout = 300_000;
 
 const maxHandlerRetries = 5;
 
