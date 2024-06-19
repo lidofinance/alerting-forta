94d880cc2a5df18b86468dc04261b5cba21ebfdd
Alexander Belokon <ikatamarinaki@gmail.com>
2024-06-19 11:58:56 +0200

    fix: remove local write-version, remove .nvmrc

--- bsc-adi-governance/src/version.ts
+++ bsc-adi-governance/src/version.ts
@@ -1,27 +1,27 @@
-import path from "path";
+import path from 'path'
 
 export interface Version {
-  desc: string;
-  commitHash: string;
-  commitHashShort: string;
-  commitMsg: string;
-  commitMsgShort: string;
-  isWdClean: boolean;
+  desc: string
+  commitHash: string
+  commitHashShort: string
+  commitMsg: string
+  commitMsgShort: string
+  isWdClean: boolean
 }
 
-export default readVersion(path.join(__dirname, "..", "version.json"));
+export default readVersion(path.join(__dirname, '..', './version.json'))
 
 function readVersion(versionFilePath: string): Version {
   try {
-    return require(versionFilePath);
+    return require(versionFilePath)
   } catch (e) {
     return {
-      desc: "unknown",
-      commitHash: "unknown",
-      commitHashShort: "unknown",
-      commitMsg: "unknown",
-      commitMsgShort: "unknown",
+      desc: 'unknown',
+      commitHash: 'unknown',
+      commitHashShort: 'unknown',
+      commitMsg: 'unknown',
+      commitMsgShort: 'unknown',
       isWdClean: false,
-    };
+    }
   }
 }
