7619627b1af5edbe082bacea872cf079db96238b
F4ever <raman.s@lido.fi>
2024-08-26 02:53:24 +0200

    feat: actualize Lido Oracle addresses

--- ethereum-validators-set/src/subagents/accounting-oracle/constants.holesky.ts
+++ ethereum-validators-set/src/subagents/accounting-oracle/constants.holesky.ts
@@ -11,14 +11,16 @@ export const ACCOUNTING_ORACLE_ADDRESS = accountingOracleAddress;
 export const ACCOUNTING_HASH_CONSENSUS_ADDRESS = accountingHashConsensusAddress;
 
 export const ACCOUNTING_ORACLE_MEMBERS = new Map<string, string>([
-  ["0xa8af49fb44aaa8eeca9ae918bb7c05e2e71c9de9", "Chorus One"],
+  ["0xf0f23944efc5a63c53632c571e7377b85d5e6b6f", "Chorus One"],
   ["0xb29dd2f6672c0dff2d2f173087739a42877a5172", "Staking Facilities"],
-  ["0xfda7e01b2718c511bf016030010572e833c7ae6a", "P2P Validator"],
+  ["0x31fa51343297ffce0cc1e67a50b2d3428057d1b1", "P2P Validator"],
   ["0xd3b1e36a372ca250eeff61f90e833ca070559970", "Stakefish"],
-  ["0x3799bda7b884d33f79cec926af21160dc47fbe05", "Rated"],
   ["0x4c75fa734a39f3a21c57e583c1c29942f021c6b7", "bloXroute"],
   ["0x81e411f1bfda43493d7994f82fb61a415f6b8fd4", "Instadapp"],
   ["0x3ff28f2ede8358e288798afc23ee299a503ad5c9", "Kyber Network"],
+  ["0xf7ae520e99ed3c41180b5e12681d31aa7302e4e5", "Chainlayer"],
+  ["0x12a1d74f8697b9f4f1eebb0a9d0fB6a751366399", "Lido"],
+  ["0xd892c09b556b547c80b7d8c8cb8d75bf541B2284", "Lido Valset"],
 ]);
 
 export const ACCOUNTING_HASH_CONSENSUS_EVENTS_OF_NOTICE =
