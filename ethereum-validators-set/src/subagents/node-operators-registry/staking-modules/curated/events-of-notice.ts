77cf7db2c60db1bbe386da55b06a98e67d700a18
Alexander Movsunov <alex.mov@lido.fi>
2024-09-30 21:29:55 +0300

    fix(val-947): event signature fixed

--- ethereum-validators-set/src/subagents/node-operators-registry/staking-modules/curated/events-of-notice.ts
+++ ethereum-validators-set/src/subagents/node-operators-registry/staking-modules/curated/events-of-notice.ts
@@ -13,7 +13,7 @@ export const getEventsOfNoticeForStakingModule = (
     {
       address: moduleAddress,
       event:
-        "event TargetValidatorsCountChanged(uint256 indexed nodeOperatorId, uint256 targetValidatorsCount)",
+        "event TargetValidatorsCountChanged(uint256 indexed nodeOperatorId, uint256 targetValidatorsCount, uint256 targetLimitMode)",
       alertId: `${alertPrefix}NODE-OPERATOR-TARGET-VALIDATORS-COUNT-CHANGED`,
       name: `⚠️ ${moduleName}: Node operator target validators count changed`,
       description: (args: any) =>
