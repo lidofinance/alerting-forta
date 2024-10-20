1b4408a53426e7eada0687eae0df1317da1a4f64
Alexander Movsunov <alex.mov@lido.fi>
2024-10-20 23:08:12 +0300

    fix(val-947): tests

--- ethereum-validators-set/e2e/agent-node-operators-registry.e2e-spec.ts
+++ ethereum-validators-set/e2e/agent-node-operators-registry.e2e-spec.ts
@@ -61,7 +61,7 @@ describe("agent-node-operators-registry e2e tests", () => {
     async () => {
       const findings = await runTransaction(
         "0xcd406d8439cf7b635ede687ea4fbe6d3e3a7d33e1a16c78ca5ba304ac06cb415",
-        19454560,
+        18293300,
       );
       expect(findings).toMatchSnapshot();
     },
@@ -84,7 +84,7 @@ describe("agent-node-operators-registry e2e tests", () => {
     async () => {
       const findings = await runTransaction(
         "0xdc70082a674abb83a94f04ea5b083849ff476ddc2f0fdd2a1cd2f7d2e079592b",
-        19454560,
+        19454500,
       );
       expect(findings).toMatchSnapshot();
     },
