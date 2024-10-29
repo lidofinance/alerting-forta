3964ea9e3b470a451bf9836dcc0a24f7cbe3e55b
Alexander Movsunov <alex.mov@lido.fi>
2024-10-29 10:27:08 +0300

    fix: split tests

--- ethereum-validators-set/e2e/agent-node-operators-registry-validators.e2e-spec.ts
+++ ethereum-validators-set/e2e/agent-node-operators-registry-validators.e2e-spec.ts
@@ -46,11 +46,10 @@ describe("agent-node-operators-registry e2e tests", () => {
   });
 
   it(
-    "should process tx with set NO target limit set",
+    "should process tx with removed signing keys",
     async () => {
       const findings = await runTransaction(
-        "0xcd406d8439cf7b635ede687ea4fbe6d3e3a7d33e1a16c78ca5ba304ac06cb415",
-        18293300,
+        "0x43529ea1c44b6f6134fa045f6af81a1a15be6309a32424ea742fa056977c4010",
       );
       expect(findings).toMatchSnapshot();
     },
@@ -58,22 +57,11 @@ describe("agent-node-operators-registry e2e tests", () => {
   );
 
   it(
-    "should process tx with added Node operator",
+    "should process tx with a lot of validators exited",
     async () => {
       const findings = await runTransaction(
-        "0xa4629245311d93a11cedb9143d8b7530057685b4b568a026bac194e162002c13",
-      );
-      expect(findings).toMatchSnapshot();
-    },
-    TEST_TIMEOUT,
-  );
-
-  it(
-    "should process tx with setting node operator name",
-    async () => {
-      const findings = await runTransaction(
-        "0x14bd64e4262041d762de83edce9a0b4c88dbb49ef678e3fd524cdc4cfdc3d88c",
-        20597472,
+        "0xdc70082a674abb83a94f04ea5b083849ff476ddc2f0fdd2a1cd2f7d2e079592b",
+        19454500,
       );
       expect(findings).toMatchSnapshot();
     },
