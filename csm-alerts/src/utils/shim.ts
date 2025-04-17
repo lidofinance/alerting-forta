6aaf8f38576a7e99bce9044d04479545447c5c17
madlabman <10616301+madlabman@users.noreply.github.com>
2025-04-17 19:03:30 +0200

    feat: add NATS shim

--- csm-alerts/src/utils/shim.ts
+++ csm-alerts/src/utils/shim.ts
@@ -9,19 +9,84 @@ import {
     TransactionEvent as TransactionEventV1,
 } from 'forta-agent'
 
-export function blockEventV1toV2(
-    blockEvent: BlockEventV1,
-    chainId: number | undefined,
+import { BlockEvent } from '../shared/nats'
+
+export function blockEventV1toV2(blockEvent: BlockEventV1, chainId: number = 1): BlockEventV2 {
+    return new BlockEventV2(chainId, blockEvent.block)
+}
+
+export function onchainMonBlockToBlockEventV2(
+    blockEvent: BlockEvent,
+    chainId: number = 1,
 ): BlockEventV2 {
-    return new BlockEventV2(chainId ?? 1, blockEvent.block)
+    return new BlockEventV2(chainId, {
+        difficulty: '',
+        extraData: '',
+        gasLimit: '',
+        gasUsed: '',
+        hash: blockEvent.hash,
+        logsBloom: '',
+        miner: '',
+        mixHash: '',
+        nonce: '',
+        number: blockEvent.number,
+        parentHash: blockEvent.parentHash,
+        receiptsRoot: '',
+        sha3Uncles: '',
+        size: '',
+        stateRoot: '',
+        timestamp: blockEvent.timestamp,
+        totalDifficulty: '',
+        transactions: blockEvent.receipts.map((r) => r.transactionHash),
+        transactionsRoot: '',
+        uncles: [],
+    })
+}
+
+export function onchainMonBlockToListTransactionEventV2(
+    blockEvent: BlockEvent,
+    chainId: number = 1,
+): TransactionEventV2[] {
+    const { hash: blockHash, number: blockNumber, timestamp } = blockEvent
+
+    return blockEvent.receipts.map(({ from, logs, to, transactionHash }) => {
+        const addresses: Record<string, true> = { [from]: true }
+        if (to) addresses[to] = true
+
+        return new TransactionEventV2(
+            chainId,
+            {
+                hash: transactionHash,
+                from,
+                to: to ?? null,
+                nonce: -1, // unknown
+                gas: '',
+                gasPrice: '',
+                value: '',
+                data: '',
+                r: '',
+                s: '',
+                v: '',
+            },
+            [], // traces
+            addresses,
+            {
+                hash: blockHash,
+                number: blockNumber,
+                timestamp,
+            },
+            logs,
+            null, // TODO: resolve create‑address once needed
+        )
+    })
 }
 
 export function txEventV1toV2(
     txEvent: TransactionEventV1,
-    chainId: number | undefined,
+    chainId: number = 1,
 ): TransactionEventV2 {
     return new TransactionEventV2(
-        chainId ?? 1,
+        chainId,
         txEvent.transaction,
         [], // traces
         txEvent.addresses,
